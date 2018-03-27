320552905b4128cb29c5f6d56714d28d3d2f0e48
Mukesh Sharma <remail2mukesh@gmail.com>
2018-03-27 12:05:47 +0530

    updated test cases as per review

--- packages/editable-html/src/plugins/list/__test__/index.test.js
+++ packages/editable-html/src/plugins/list/__test__/index.test.js
@@ -1,152 +1,54 @@
 import React from "react";
 
-import {configure} from "enzyme";
+import {configure, shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-15";
 import renderer from 'react-test-renderer';
-import List from './../../list/index';
+import List, {serialization} from '../index';
 import Util from '../../utils';
 
 configure({adapter : new Adapter() });
 
 describe("ListPlugin", () => {
-  let list, ListPlugin, toolbar;
-  beforeEach(() => {
-    list = require('../index');
-    ListPlugin = list.default;
-    toolbar = list.toolbar
-  });
 
   describe("deserialize", () => {
-    it("should deserialize list item", () => {
-      let el = {
-        tagName : 'li',
-        childNodes : []
-      }
-      const next = jest.fn();
-      const out = list.serialization.deserialize(el, next);
 
-      expect(out).toMatchObject({object : 'block', type : 'list_item', nodes: undefined});
-    });
-
-    it("should deserialize ul", () => {
-      let el = {
-        tagName : 'ul',
-        childNodes : []
-      }
-      const next = jest.fn();
-      const out = list.serialization.deserialize(el, next);
-
-      expect(out).toMatchObject({object : 'block', type : 'ul_list', nodes: undefined});
-    });    
-
-    it("should deserialize ol", () => {
-      let el = {
-        tagName : 'ol',
-        childNodes : []
-      }
-      const next = jest.fn();
-      const out = list.serialization.deserialize(el, next);
-
-      expect(out).toMatchObject({object : 'block', type : 'ol_list', nodes: undefined});
-    });        
-  });
+    const assertDeserialize = (tagName, expectedType) => {
+      it(`should deserialize ${tagName} to ${expectedType}`, () => {
+        const out = serialization.deserialize({tagName}, jest.fn());
+        expect(out).toMatchObject({object : 'block', type : expectedType});
+      });
+    }
+    assertDeserialize('ul', 'ul_list');
+    assertDeserialize('ol', 'ol_list');
+    assertDeserialize('li', 'list_item');
+  });  
 
   describe("serialize", () => {
-    it("should serialize list item", () => {
-      let el = {
-        block : 'block',
-        type : 'list_item'
-        
-      }
-      const out = list.serialization.serialize(el, {
-        type:'div',
-        text : 'hello'
-      });
 
-      expect(out.type).toMatch('li');
-    });    
-
-    it("should serialize ul", () => {
-      let el = {
-        block : 'block',
-        type : 'ul_list'
-        
-      }
-      const out = list.serialization.serialize(el, {
-        type:'div',
-        text : 'hello'
-      });
-
-      expect(out.type).toMatch('ul');
-    });    
-    
-    it("should serialize ol", () => {
-      let el = {
-        block : 'block',
-        type : 'ol_list'
-        
-      }
-      const out = list.serialization.serialize(el, {
-        type:'div',
-        text : 'hello'
-      });
-
-      expect(out.type).toMatch('ol');
-    });        
-  });
+    const assertSerialize = (type, expectedType) => {
+      it(`should serialize ${type} to ${expectedType}`, () => {
+        const out = serialization.serialize({block : 'block', type: type}, {});
+        expect(out.type).toMatch(expectedType);
+      });      
+    }
+    assertSerialize('ul_list', 'ul');
+    assertSerialize('ol_list', 'ol');
+    assertSerialize('list_item', 'li');
+  });  
+  
   describe("renderNode", () => {
-    it("should render list item node", () => {
-      let obj = {
-        node : {
-          type : 'list_item'
-        },
-        attributes : {},
-        children : 'icon'
-      }
+    let plugin = List({});
 
-      let plugin = ListPlugin({});
-      const out = plugin.renderNode(obj);
-      expect(out.type).toMatch('li');
-    });
+    const assertRenderNode = (type, expectedType) => {
+      it(`should renderNode ${type} to ${expectedType}`, () => {
+        const out = plugin.renderNode({node: {type}});
+        expect(out.type).toMatch(expectedType);
+      });
+    }
 
-    it("should render ul node", () => {
-      let obj = {
-        node : {
-          type : 'ul_list'
-        },
-        attributes : {},
-        children : 'icon'
-      }
-
-      let plugin = ListPlugin({});
-      const out = plugin.renderNode(obj);
-      expect(out.type).toMatch('ul');
-    });
-    
-    it("should render list item node", () => {
-      let obj = {
-        node : {
-          type : 'ol_list'
-        },
-        attributes : {},
-        children : 'icon'
-      }
-
-      let plugin = ListPlugin({});
-      const out = plugin.renderNode(obj);
-      expect(out.type).toMatch('ol');
-    });    
-  });  
-
-  describe("toolbar", () => {
-    describe('onClick toolbar ul', () => {
-     
-     it("should onClick ul", () => {
-      
-      let plugin = ListPlugin({});
-
-     })
-    });
-  });  
+    assertRenderNode('ul_list', 'ul');
+    assertRenderNode('ol_list', 'ol');
+    assertRenderNode('list_item', 'li');
+  });
   
 });
