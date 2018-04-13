0b26a70c2e979c572db7aa2f0c81d4cfca7a713c
edeustace <ed.eustace@gmail.com>
2018-04-13 14:52:33 -0400

    chore(test): fix tests

--- packages/editable-html/src/__test__/utils.js
+++ packages/editable-html/src/__test__/utils.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export function classObject() {
-  const out = {}
+  const out = {};
   for (var a in arguments) {
     const value = arguments[a];
     out[value] = value;
@@ -10,28 +10,34 @@ export function classObject() {
 }
 
 export function mockComponents() {
-  const out = {}
+  const out = {};
   for (var a in arguments) {
     const value = arguments[a];
-    out[value] = () => <div data-mock-component="true">{value}</div>;
+    out[value] = class extends React.Component {
+      render() {
+        return <div data-mock-component="true">{value}</div>;
+      }
+    };
   }
   return out;
 }
 
 export function mockIconButton() {
   jest.mock('material-ui/IconButton', () => {
-    return (props) => <div className={props.className} style={props.style} ariaLabel={props['aria-label']}></div>
+    return props => (
+      <div
+        className={props.className}
+        style={props.style}
+        ariaLabel={props['aria-label']}
+      />
+    );
   });
 }
 
 export function mockMathInput() {
-  jest.mock(
-    '@pie-lib/math-input',
-    () => ({
-      addBrackets: jest.fn((s) => s),
-      removeBrackets: jest.fn(s => s),
-      ...mockComponents('Keypad', 'MathQuillInput', 'EditableMathInput')
-    }));
+  jest.mock('@pie-lib/math-input', () => ({
+    addBrackets: jest.fn(s => s),
+    removeBrackets: jest.fn(s => s),
+    ...mockComponents('Keypad', 'MathQuillInput', 'EditableMathInput')
+  }));
 }
-
-
--- packages/editable-html/src/plugins/list/__test__/index.test.js
+++ packages/editable-html/src/plugins/list/__test__/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 import List, { serialization } from '../index';
 import Util from '../../utils';
