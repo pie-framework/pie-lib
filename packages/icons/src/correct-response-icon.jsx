fbd4556e26863e2c8e097e6739808cbe44cdcec6
edeustace <ed.eustace@gmail.com>
2018-04-19 12:53:23 -0400

    fix(correct-response): fix styles import

--- packages/icons/src/correct-response-icon.jsx
+++ packages/icons/src/correct-response-icon.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
-import withStyles from 'material-ui/styles';
+import { withStyles } from 'material-ui/styles';
 
 const OpenIcon = ({ bg, fg }) => (
   <svg
@@ -104,6 +104,7 @@ const CorrectResponse = ({ open, classes, className }) => (
 );
 
 CorrectResponse.propTypes = {
+  classes: PropTypes.object.isRequired,
   open: PropTypes.bool,
   size: PropTypes.string,
   className: PropTypes.string
