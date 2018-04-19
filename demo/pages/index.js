0feea9d9f3ddd7721c67242ef1cf77863998bc92
edeustace <ed.eustace@gmail.com>
2018-04-18 23:26:21 -0400

    polish demo

--- demo/pages/index.js
+++ demo/pages/index.js
@@ -1,1 +1,15 @@
-export default () => <div>foo</div>;
+import { withStyles } from 'material-ui/styles';
+import React from 'react';
+
+import withRoot from '../src/withRoot';
+const D = ({ classes }) => (
+  <div className={classes.root}>foo where is the container</div>
+);
+
+export default withRoot(
+  withStyles({
+    root: {
+      backgroundColor: 'blue'
+    }
+  })(D)
+);
