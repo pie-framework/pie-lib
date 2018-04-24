6aebd59de990921f12e26d23d48a9f64afd4b7ba
edeustace <ed.eustace@gmail.com>
2018-04-24 14:21:22 -0400

    chore(lint): lint choice-configuration

--- packages/config-ui/src/choice-configuration/__tests__/feedback-menu.test.jsx
+++ packages/config-ui/src/choice-configuration/__tests__/feedback-menu.test.jsx
@@ -3,10 +3,9 @@ import FeedbackMenu from '../feedback-menu';
 import renderer from 'react-test-renderer';
 
 describe('feedback-menu', () => {
-
   it('renders correctly', () => {
     const tree = renderer
-      .create(<FeedbackMenu/>)
+      .create(<FeedbackMenu onChange={jest.fn()} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
--- packages/config-ui/src/choice-configuration/feedback-menu.jsx
+++ packages/config-ui/src/choice-configuration/feedback-menu.jsx
@@ -5,23 +5,27 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export class IconMenu extends React.Component {
+  static propTypes = {
+    opts: PropTypes.object,
+    onClick: PropTypes.func.isRequired,
+    iconButtonElement: PropTypes.any
+  };
+
   constructor(props) {
     super(props);
     this.state = {
       anchorEl: undefined,
       open: false
     };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleRequestClose = this.handleRequestClose.bind(this);
   }
 
-  handleClick(event) {
+  handleClick = event => {
     this.setState({ open: true, anchorEl: event.currentTarget });
-  }
+  };
 
-  handleRequestClose() {
+  handleRequestClose = () => {
     this.setState({ open: false });
-  }
+  };
 
   render() {
     const { opts, onClick } = this.props;
@@ -29,7 +33,7 @@ export class IconMenu extends React.Component {
     const keys = Object.keys(opts);
 
     const handleMenuClick = key => () => {
-      this.props.onClick(key);
+      onClick(key);
       this.handleRequestClose();
     };
 
@@ -53,44 +57,47 @@ export class IconMenu extends React.Component {
   }
 }
 
-IconMenu.propTypes = {
-  iconButtonElement: PropTypes.any
-};
+export default class FeedbackMenu extends React.Component {
+  static propTypes = {
+    value: PropTypes.object,
+    onChange: PropTypes.func.isRequired,
+    classes: PropTypes.object.isRequired
+  };
 
-/**
- * TODO: move FeedbackMenu to config-ui (there's one in multiple choice too).
- */
-export default function FeedbackMenu(props) {
-  const { value, onChange, classes } = props;
+  static defaultProps = {
+    classes: {}
+  };
 
-  const t = value && value.type;
-  const iconColor =
-    t === 'custom' ? 'secondary' : t === 'default' ? 'primary' : 'disabled';
+  render() {
+    const { value, onChange, classes } = this.props;
 
-  const tooltip =
-    t === 'custom'
-      ? 'Custom Feedback'
-      : t === 'default' ? 'Default Feedback' : 'Feedback disabled';
+    const t = value && value.type;
+    const iconColor =
+      t === 'custom' ? 'secondary' : t === 'default' ? 'primary' : 'disabled';
 
-  const icon = (
-    <IconButton className={classes.icon} aria-label={tooltip}>
-      <ActionFeedback color={iconColor} />
-    </IconButton>
-  );
+    const tooltip =
+      t === 'custom'
+        ? 'Custom Feedback'
+        : t === 'default'
+          ? 'Default Feedback'
+          : 'Feedback disabled';
 
-  return (
-    <IconMenu
-      iconButtonElement={icon}
-      onClick={key => onChange(key)}
-      opts={{
-        none: 'No Feedback',
-        default: 'Default',
-        custom: 'Custom'
-      }}
-    />
-  );
+    const icon = (
+      <IconButton className={classes.icon} aria-label={tooltip}>
+        <ActionFeedback color={iconColor} />
+      </IconButton>
+    );
+
+    return (
+      <IconMenu
+        iconButtonElement={icon}
+        onClick={key => onChange(key)}
+        opts={{
+          none: 'No Feedback',
+          default: 'Default',
+          custom: 'Custom'
+        }}
+      />
+    );
+  }
 }
-
-FeedbackMenu.defaultProps = {
-  classes: {}
-};
