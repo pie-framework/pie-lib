4d3e3ae2523328cf049860c346d45d59c10be358
edeustace <ed.eustace@gmail.com>
2018-05-03 15:10:04 -0400

    chore(demo): add formatting components

---
+++ demo/src/formatting/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+import _ from 'lodash';
+
+const comp = (variant, styles) => {
+  const out = withStyles(theme => {
+    styles = _.isFunction(styles) ? styles(theme) : styles;
+    return {
+      comp: {
+        paddingTop: theme.spacing.unit,
+        paddingBottom: theme.spacing.unit,
+        ...styles
+      }
+    };
+  })(({ children, classes }) => (
+    <Typography className={classes.comp} variant={variant}>
+      {children}
+    </Typography>
+  ));
+
+  out.propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node
+    ]).isRequired
+  };
+
+  return out;
+};
+
+export const Body = comp('body1', { paddingTop: '0px' });
+export const Header = comp('title');
