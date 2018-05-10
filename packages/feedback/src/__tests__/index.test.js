6329fb68a08fbbc7f7c98276905082da3184826b
edeustace <ed.eustace@gmail.com>
2018-05-09 21:51:01 -0400

    feat(getFeedback): add getFeedback function

--- packages/feedback/src/__tests__/index.test.js
+++ packages/feedback/src/__tests__/index.test.js
@@ -1,20 +1,61 @@
+import { getFeedback } from '../index';
 describe('feedback', () => {
   describe('getFeedback', () => {
-    const assert = () => {
-      it(` -> ${expected}`, async (correctness, config, expected) => {
+    const assert = (correctness, config, expected) => {
+      it(`${correctness} + ${JSON.stringify(
+        config
+      )} -> ${expected}`, async () => {
         const result = await getFeedback(correctness, config);
         expect(result).toEqual(expected);
       });
     };
 
-    assert(
-      'correct',
-      {
-        correct: {
-          type: 'default'
-        }
-      },
-      'Correct'
-    );
+    describe('correct', () => {
+      assert('correct', {}, 'Correct');
+      assert('correct', { correct: { type: 'default' } }, 'Correct');
+      assert('correct', { correct: { type: 'default', default: 'C' } }, 'C');
+      assert('correct', { correct: { type: 'none', default: 'C' } }, undefined);
+      assert(
+        'correct',
+        { correct: { type: 'custom', custom: 'Custom' } },
+        'Custom'
+      );
+    });
+
+    describe('incorrect', () => {
+      assert('incorrect', {}, 'Incorrect');
+      assert('incorrect', { incorrect: { type: 'default' } }, 'Incorrect');
+      assert(
+        'incorrect',
+        { incorrect: { type: 'default', default: 'I' } },
+        'I'
+      );
+      assert(
+        'incorrect',
+        { incorrect: { type: 'none', default: 'C' } },
+        undefined
+      );
+      assert(
+        'incorrect',
+        { incorrect: { type: 'custom', custom: 'Custom' } },
+        'Custom'
+      );
+    });
+
+    describe('unknown', () => {
+      assert('unknown', {}, undefined);
+    });
+
+    describe('partial', () => {
+      assert('partial', {}, 'Nearly');
+      assert('partial', { partial: { type: 'default' } }, 'Nearly');
+      assert('partial', { partial: { type: 'default', default: 'I' } }, 'I');
+      assert('partial', { partial: { type: 'none', default: 'C' } }, undefined);
+      assert(
+        'partial',
+        { partial: { type: 'custom', custom: 'Custom' } },
+        'Custom'
+      );
+    });
   });
 });
--- packages/feedback/src/index.js
+++ packages/feedback/src/index.js
@@ -1,4 +1,38 @@
+const defaults = {
+  correct: { type: 'default', default: 'Correct', custom: 'Correct' },
+  incorrect: { type: 'default', default: 'Incorrect', custom: 'Incorrect' },
+  partial: { type: 'default', default: 'Nearly', custom: 'Nearly' }
+};
+
+/**
+ * @typedef {Object} FeedbackConfig
+ * @property {'default'|'none'|'custom'} type
+ * @property {string} default
+ * @property {string} custom
+ *
+ * @typedef {Object} Feedback
+ *  @property {FeedbackConfig} correct
+ *  @property {FeedbackConfig} incorrect
+ *  @property {FeedbackConfig} partial
+ */
+
+/**
+ * Get the feedback for the correctness
+ *
+ * @param {'correct'|'incorrect'|'partial'} correctness
+ * @param {Feedback} feedback
+ */
 export const getFeedback = (correctness, feedback) =>
-  new Promise((resolve, reject) => {
-    resolve('foo');
+  new Promise(resolve => {
+    const fb = feedback[correctness] || defaults[correctness] || {};
+
+    if (fb.type === 'default') {
+      resolve(fb.default || defaults[correctness].default);
+    } else if (fb.type === 'custom') {
+      resolve(fb.custom || defaults[correctness].custom);
+    } else if (fb.type === 'none') {
+      resolve(undefined);
+    } else {
+      resolve(undefined);
+    }
   });
