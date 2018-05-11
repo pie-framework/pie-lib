a6835c4f026a92bb29316838aee7c31d5a895285
edeustace <ed.eustace@gmail.com>
2018-05-10 21:18:13 -0400

    feat(unanswered): add unanswered feedback

--- packages/feedback/src/index.js
+++ packages/feedback/src/index.js
@@ -1,7 +1,12 @@
 export const defaults = {
   correct: { type: 'default', default: 'Correct', custom: 'Correct' },
   incorrect: { type: 'default', default: 'Incorrect', custom: 'Incorrect' },
-  partial: { type: 'default', default: 'Nearly', custom: 'Nearly' }
+  partial: { type: 'default', default: 'Nearly', custom: 'Nearly' },
+  unanswered: {
+    type: 'default',
+    default: 'You have not entered a response',
+    custom: 'You have not entered a response'
+  }
 };
 
 /**
