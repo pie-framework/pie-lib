352600c3f264857719899905121b472f43c802c2
edeustace <ed.eustace@gmail.com>
2018-05-16 10:58:58 -0400

    fix(test): fix up tests

--- packages/charting-config/src/__tests__/configure.test.js
+++ packages/charting-config/src/__tests__/configure.test.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import DisplayConfig from '../display-config';
 import GraphAttributeConfig from '../graph-attribute-config';
-import Input from 'material-ui/Input';
-import Button from 'material-ui/Button';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 import { InputRadio, InputCheckbox, InputContainer } from '@pie-lib/config-ui';
+import * as configUi from '@pie-lib/config-ui';
 import Box from '../box';
 import { shallowChild } from '@pie-lib/test-utils';
 
@@ -21,7 +22,7 @@ const defaultProps = {
       partialFeedbackType: 'none',
       partialFeedbackValue: '',
       incorrectFeedbackType: 'none',
-      incorrectFeedbackValue: '',
+      incorrectFeedbackValue: ''
     },
     model: {
       config: {
--- packages/scoring-config/__mocks__/@pie-lib/config-ui.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const NumberTextField = () => <div>NumberTextField</div>;
