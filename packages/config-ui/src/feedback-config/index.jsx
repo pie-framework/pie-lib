6bffd28d1718aec6acb1bcbf18a8bb3a26b27ef2
Elod-Arpad Szopos <elodszopos@hotmail.com>
2018-06-29 15:26:37 +0300

    feat(config-ui): update feedback ui for config views [ch83]

--- packages/config-ui/src/feedback-config/index.jsx
+++ packages/config-ui/src/feedback-config/index.jsx
@@ -1,13 +1,22 @@
 import FeedbackSelector, { FeedbackType } from './feedback-selector';
-import FormSection from '../form-section';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import merge from 'lodash/merge';
 
 export { FeedbackSelector };
 
-const style = {};
+const style = {
+  feedbackContainer: {
+    display: 'flex',
+    flexDirection: 'column'
+  }
+};
 
 export const buildDefaults = input => {
   return merge(
@@ -29,7 +38,8 @@ export class FeedbackConfig extends React.Component {
       incorrect: PropTypes.shape(FeedbackType),
       partial: PropTypes.shape(FeedbackType)
     }),
-    onChange: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired,
+    classes: PropTypes.object.isRequired
   };
 
   static defaultProps = {
@@ -48,27 +58,35 @@ export class FeedbackConfig extends React.Component {
   onPartialChange = this.onChange.bind(this, 'partial');
 
   render() {
-    const { allowPartial, feedback } = this.props;
+    const { classes, allowPartial, feedback } = this.props;
+
     return (
-      <FormSection label="Feedback">
-        <FeedbackSelector
-          label="If correct, show"
-          feedback={feedback.correct}
-          onChange={this.onCorrectChange}
-        />
-        {allowPartial && (
-          <FeedbackSelector
-            label="If partially correct, show"
-            feedback={feedback.partial}
-            onChange={this.onPartialChange}
-          />
-        )}
-        <FeedbackSelector
-          label="If incorrect, show"
-          feedback={feedback.incorrect}
-          onChange={this.onIncorrectChange}
-        />
-      </FormSection>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>Feedback</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <div className={classes.feedbackContainer}>
+            <FeedbackSelector
+              label="If correct, show"
+              feedback={feedback.correct}
+              onChange={this.onCorrectChange}
+            />
+            {allowPartial && (
+              <FeedbackSelector
+                label="If partially correct, show"
+                feedback={feedback.partial}
+                onChange={this.onPartialChange}
+              />
+            )}
+            <FeedbackSelector
+              label="If incorrect, show"
+              feedback={feedback.incorrect}
+              onChange={this.onIncorrectChange}
+            />
+          </div>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
     );
   }
 }
