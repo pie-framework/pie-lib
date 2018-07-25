29274a6c9a75520fc880eb119e5de64de0e887e9
edeustace <ed.eustace@gmail.com>
2018-07-25 10:24:33 -0400

    rm logs

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,3 @@
-console.log('------------ jest.setup.js');
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
-console.log('------------ enzyme configured with react 16 adapter');
