92c5ddc4fe22300af154985cf9083b7c2bcac21d
edeustace <ed.eustace@gmail.com>
2018-08-28 11:50:42 +0100

    chore: test displaystyle in mathquill

--- packages/editable-html/playground/mathquill/index.jsx
+++ packages/editable-html/playground/mathquill/index.jsx
@@ -1,60 +1,147 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Static from '../../src/plugins/math/mathquill/static';
-import { MathToolbar } from '../../src/plugins/math/math-toolbar';
-const Button = props => (
-  <div
-    style={{ border: 'solid red 1px', padding: '20px' }}
-    onClick={() => props.onClick(props.uid, props.latex)}
-  >
-    <Static latex={props.latex} />
-  </div>
-);
+import PropTypes from 'prop-types';
+import debug from 'debug';
+import { debounce } from 'lodash';
+
+let MQ;
+if (typeof window !== 'undefined') {
+  const MathQuill = require('mathquill');
+  MQ = MathQuill.getInterface(2);
+}
+
+const log = debug('@pie-lib:editable-html:plugins:math:mathquill:editor');
+
+export default class Editor extends React.Component {
+  static propTypes = {
+    onClick: PropTypes.func,
+    onChange: PropTypes.func.isRequired,
+    latex: PropTypes.string.isRequired,
+    onFocus: PropTypes.func,
+    onBlur: PropTypes.func,
+    className: PropTypes.string
+  };
+
+  componentDidMount() {
+    if (!MQ) {
+      throw new Error('MQ is not defined - but component has mounted?');
+    }
+    this.mathField = MQ.MathField(this.input, {
+      handlers: {
+        edit: this.onInputEdit.bind(this)
+      }
+    });
+    this.mathField.latex(this.props.latex);
+  }
+
+  componentDidUpdate() {
+    log('[componentDidUpdate] latex: ', this.props.latex);
+    if (this.mathField) {
+      this.mathField.latex(this.props.latex);
+    }
+  }
+
+  clear() {
+    this.mathField.latex('');
+    return '';
+  }
+
+  blur() {
+    log('blur mathfield');
+    this.mathField.blur();
+  }
+
+  focus() {
+    log('focus mathfield...');
+    this.mathField.focus();
+  }
+
+  command(v) {
+    this.mathField.cmd(v);
+    this.mathField.focus();
+    return this.mathField.latex();
+  }
+
+  keystroke(v) {
+    this.mathField.keystroke(v);
+    this.mathField.focus();
+    return this.mathField.latex();
+  }
+
+  write(v) {
+    this.mathField.write(v);
+    this.mathField.focus();
+    return this.mathField.latex();
+  }
+
+  latex() {
+    if (this.mathField) {
+      return this.mathField.latex();
+    }
+  }
+
+  _onInputEdit = event => {
+    log('[onInputEdit] ...', event);
+
+    if (!this.mathField) {
+      return;
+    }
+    if (this.latexIsEqual(this.mathField.latex(), this.props.latex)) {
+      return;
+    }
+    this.props.onChange(this.mathField.latex());
+  };
+
+  onInputEdit = debounce(this._onInputEdit, 300, {
+    leading: false,
+    trailing: true
+  });
+
+  latexIsEqual = (a, b) => {
+    if (!a && !b) {
+      return true;
+    }
+    if ((a && !b) || (!a && b)) {
+      return false;
+    }
+    return a.trim().replace(/\s/g, '') === b.trim().replace(/\s/, '');
+  };
+
+  shouldComponentUpdate(nextProps) {
+    log(
+      '[shouldComponentUpdate] nextProps.latex: ',
+      nextProps.latex,
+      'current: ',
+      this.mathField.latex()
+    );
+    const isEqual = this.latexIsEqual(nextProps.latex, this.mathField.latex());
+    log('[shouldComponentUpdate] isEqual? ', isEqual);
+    return !isEqual;
+  }
+
+  render() {
+    log('[render] this.props.latex:', this.props.latex);
+    const { onClick, onFocus, onBlur, className } = this.props;
+
+    return (
+      <span
+        onClick={onClick}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        className={className}
+        ref={r => (this.input = r)}
+      />
+    );
+  }
+}
 
 class Demo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      currentIndex: 0,
-      math: [{ id: 1, latex: '\\frac{1}{2}' }, { id: 2, latex: '\\frac{2}{1}' }]
-    };
   }
-
-  onDone = (id, latex) => {
-    const update = this.state.math.map(m => {
-      if (m.id === id) {
-        m.latex = latex;
-      }
-      return m;
-    });
-
-    this.setState({ math: update });
-  };
-
-  loadLatex = index => {
-    this.setState({ currentIndex: index });
-  };
-
   render() {
-    const data = this.state.math[this.state.currentIndex];
-    console.log('RENDER, data:', data);
-    return (
-      <div>
-        mathquill demo
-        {this.state.math.map((m, index) => (
-          <Button
-            key={index}
-            uid={m.id}
-            latex={m.latex}
-            onClick={() => this.loadLatex(index)}
-          />
-        ))}
-        <MathToolbar
-          latex={data.latex}
-          onDone={this.onDone.bind(this, data.id)}
-        />
-      </div>
-    );
+    const latex = '- \\frac{1}{2}';
+    return <Editor latex={latex} onChange={() => ({})} />;
   }
 }
 
