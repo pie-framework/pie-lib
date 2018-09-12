e3f81bd7b84a831409bbe10d84c4352b5cc77b2b
edeustace <ed.eustace@gmail.com>
2018-09-12 14:41:45 +0100

    chore: fix snapshots

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   testRegex: '(/__tests__/.*\\.test.jsx|(\\.|/)(test|spec))\\.jsx?$',
   setupFiles: ['./jest.setup.js'],
   verbose: false,
-  snapshotSerializers: ['enzyme-to-json/serializer']
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testURL: 'http://localhost'
 };
