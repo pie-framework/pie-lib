e15e4a6e2601651775d6853ee9389f7d793c3cae
Elod-Arpad Szopos <elodszopos@hotmail.com>
2018-11-05 09:36:38 +0200

    feat(math-evaluator): demo page

--- demo/pages/math-evaluator/index.js
+++ demo/pages/math-evaluator/index.js
@@ -1,9 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import withRoot from '../../src/withRoot';
+import EditableHtml from '@pie-lib/editable-html';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import areValuesEqual from '../../../packages/math-evaluator/src/index';
+// import mathExpressions from 'math-expressions';
+// import escapeLatex from 'escape-latex';
+// import sanitizeLatex from 'sanitize-latex';
+import katex from 'katex';
+
+require('katex/dist/katex.css');
+
+let renderMathInElement = () => {};
+
+if (typeof window !== 'undefined') {
+  //Auto render requires the katex global
+  window.katex = katex;
+  renderMathInElement = require('katex/dist/contrib/auto-render.min');
+}
+
+const renderOpts = {
+  delimiters: [
+    { left: '\\(', right: '\\)', display: false },
+    { left: '$', right: '$', display: false }
+  ]
+};
+
+const html = '<div><span data-latex="">\\(\\frac{1}{2}\\)</span></div>';
+
+class RawMarkupPreview extends React.Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    markup: PropTypes.string.isRequired
+  };
+
+  componentDidUpdate() {
+    if (this.preview) {
+      renderMathInElement(this.preview, renderOpts);
+    }
+  }
+
+  componentDidMount() {
+    if (this.preview) {
+      renderMathInElement(this.preview, renderOpts);
+    }
+  }
+
+  render() {
+    const { markup, classes } = this.props;
+    return (
+      <div>
+        <Typography variant="title">Markup</Typography>
+        <div
+          ref={r => (this.preview = r)}
+          dangerouslySetInnerHTML={{ __html: markup }}
+        />
+        <hr />
+        <Typography variant="subheading">Raw</Typography>
+        <pre className={classes.prettyPrint}>{markup}</pre>
+        <hr />
+      </div>
+    );
+  }
+}
+const MarkupPreview = withStyles(() => ({
+  prettyPrint: {
+    whiteSpace: 'normal',
+    width: '100%'
+  }
+}))(RawMarkupPreview);
 
 class Demo extends React.Component {
   constructor(props) {
@@ -12,9 +80,19 @@ class Demo extends React.Component {
       equal: true,
       exprOne: '',
       exprTwo: '',
-      inverse: false
+      inverse: false,
+      markupOne: html,
+      markupTwo: html,
+      showHighlight: false,
+      disabled: false,
+      width: '',
+      height: ''
     };
   }
+
+  onChange = input => markup => {
+    this.setState({ [input]: markup });
+  };
 
   componentDidMount() {
     this.setState({ mounted: true });
@@ -25,7 +103,17 @@ class Demo extends React.Component {
   };
 
   render() {
-    const { mounted, equal, inverse } = this.state;
+    const {
+      markupOne,
+      markupTwo,
+      showHighlight,
+      disabled,
+      width,
+      height,
+      mounted,
+      equal,
+      inverse
+    } = this.state;
 
     return mounted ? (
       <div>
@@ -63,10 +151,44 @@ class Demo extends React.Component {
           <Button onClick={() => this.isResponseCorrect(this.state.exprOne, this.state.exprTwo)}>Evaluate</Button>
         </div>
         <Typography>Values are: <b>{equal ? ' EQUAL ' : ' NOT EQUAL '}</b></Typography>
+        <div>
+          <EditableHtml
+            markup={markupOne}
+            onChange={this.onChange('markupOne')}
+            disabled={disabled}
+            highlightShape={showHighlight}
+            width={width}
+            height={height}
+          />
+          <br />
+          <MarkupPreview markup={markupOne} />
+          <br />
+          <br />
+          <br />
+          <EditableHtml
+            markup={markupTwo}
+            onChange={this.onChange('markupTwo')}
+            disabled={disabled}
+            highlightShape={showHighlight}
+            width={width}
+            height={height}
+          />
+          <br />
+          <MarkupPreview markup={markupTwo} />
+          {markupTwo.toString()}
+        </div>
       </div>
     ) : (
       <div>loading...</div>
     );
   }
 }
-export default withRoot(Demo);
+
+const styles = theme => ({
+  sizeInput: {
+    width: '60px',
+    paddingLeft: theme.spacing.unit * 2
+  }
+});
+
+export default withRoot(withStyles(styles)(Demo));
