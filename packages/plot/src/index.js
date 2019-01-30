1adf654849a64dc86629f9a8cdec8879b711f69f
ed eustace <ed.eustace@gmail.com>
2019-01-30 22:00:10 +0000

    feat: new package plot
    Contains shared components for charting/graphing

---
+++ packages/plot/src/index.js
@@ -0,0 +1,8 @@
+export Root from './root';
+export Draggable from './draggable';
+export { gridDraggable } from './grid-draggable';
+export * as utils from './utils';
+export * as types from './types';
+import { create as createGraphProps } from './graph-props';
+
+export { createGraphProps };
