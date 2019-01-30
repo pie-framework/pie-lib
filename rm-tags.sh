d989be4c9259d5d52ca5c98616c081447455cfa8
ed eustace <ed.eustace@gmail.com>
2019-01-30 22:49:15 +0000

    rm tags

---
+++ rm-tags.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+git tag --delete @pie-lib/categorize@0.5.0
+git tag --delete @pie-lib/charting@4.0.0
+git tag --delete @pie-lib/config-ui@7.11.0
+git tag --delete @pie-lib/correct-answer-toggle@2.1.1
+git tag --delete @pie-lib/demo@1.0.1 
+git tag --delete @pie-lib/drag@1.1.3
+git tag --delete @pie-lib/editable-html@6.16.0
+git tag --delete @pie-lib/graphing@0.1.0
+git tag --delete @pie-lib/icons@2.3.0
+git tag --delete @pie-lib/math-evaluator@0.3.0
+git tag --delete @pie-lib/math-input@4.7.0
+git tag --delete @pie-lib/math-rendering@1.2.0
+git tag --delete @pie-lib/math-toolbar@0.6.0
+git tag --delete @pie-lib/plot@0.1.0
+git tag --delete @pie-lib/render-ui@4.3.0
+git tag --delete @pie-lib/scoring-config@3.3.0
+git tag --delete @pie-lib/test-utils@0.1.0
+git tag --delete @pie-lib/text-select@1.3.0
+git tag --delete @pie-lib/tools@0.8.0
+git tag --delete @pie-lib/charting@4.0.0
+git tag --delete @pie-lib/categorize@1.0.0
+git tag --delete @pie-lib/drag@2.0.0
+git tag --delete @pie-lib/correct-answer-toggle@3.0.0
+git tag --delete @pie-lib/config-ui@8.0.0
+git tag --delete @pie-lib/editable-html@7.0.0
+git tag --delete @pie-lib/graphing@1.0.0
+git tag --delete @pie-lib/icons@3.0.0
+git tag --delete @pie-lib/math-evaluator@1.0.0
+git tag --delete @pie-lib/math-input@5.0.0
+git tag --delete @pie-lib/math-rendering@2.0.0
+git tag --delete @pie-lib/math-toolbar@1.0.0 
+git tag --delete @pie-lib/plot@1.0.0
+git tag --delete @pie-lib/render-ui@5.0.0
+git tag --delete @pie-lib/scoring-config@4.0.0
+git tag --delete @pie-lib/test-utils@1.0.0
+git tag --delete @pie-lib/text-select@2.0.0
+git tag --delete @pie-lib/tools@1.0.0
+
+
+git tag --delete origin @pie-lib/categorize@0.5.0
+git tag --delete origin @pie-lib/charting@4.0.0
+git tag --delete origin @pie-lib/config-ui@7.11.0
+git tag --delete origin @pie-lib/correct-answer-toggle@2.1.1
+git tag --delete origin @pie-lib/demo@1.0.1 
+git tag --delete origin @pie-lib/drag@1.1.3
+git tag --delete origin @pie-lib/editable-html@6.16.0
+git tag --delete origin @pie-lib/graphing@0.1.0
+git tag --delete origin @pie-lib/icons@2.3.0
+git tag --delete origin @pie-lib/math-evaluator@0.3.0
+git tag --delete origin @pie-lib/math-input@4.7.0
+git tag --delete origin @pie-lib/math-rendering@1.2.0
+git tag --delete origin @pie-lib/math-toolbar@0.6.0
+git tag --delete origin @pie-lib/plot@0.1.0
+git tag --delete origin @pie-lib/render-ui@4.3.0
+git tag --delete origin @pie-lib/scoring-config@3.3.0
+git tag --delete origin @pie-lib/test-utils@0.1.0
+git tag --delete origin @pie-lib/text-select@1.3.0
+git tag --delete origin @pie-lib/tools@0.8.0
+git tag --delete origin @pie-lib/categorize@1.0.0
+git tag --delete origin @pie-lib/drag@2.0.0
+git tag --delete origin @pie-lib/correct-answer-toggle@3.0.0
+git tag --delete origin @pie-lib/config-ui@8.0.0
+git tag --delete origin @pie-lib/editable-html@7.0.0
+git tag --delete origin @pie-lib/graphing@1.0.0
+git tag --delete origin @pie-lib/icons@3.0.0
+git tag --delete origin @pie-lib/math-evaluator@1.0.0
+git tag --delete origin @pie-lib/math-input@5.0.0
+git tag --delete origin @pie-lib/math-rendering@2.0.0
+git tag --delete origin @pie-lib/plot@1.0.0
+git tag --delete origin @pie-lib/render-ui@5.0.0
+git tag --delete origin @pie-lib/scoring-config@4.0.0
+git tag --delete origin @pie-lib/test-utils@1.0.0
+git tag --delete origin @pie-lib/text-select@2.0.0
+git tag --delete origin @pie-lib/tools@1.0.0
