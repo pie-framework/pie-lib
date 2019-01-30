06a035aff8f61a221b2d8d2d9156f3fb642480de
ed eustace <ed.eustace@gmail.com>
2019-01-30 21:00:49 +0000

    fix: use links

--- packages/demo/src/withRoot.js
+++ packages/demo/src/withRoot.js
@@ -4,26 +4,7 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import getPageContext from './getPageContext';
 import Root from './root';
-
-const links = [
-  { label: 'config-ui', path: '/config-ui' },
-  { label: 'config-ui - numbers', path: '/config-ui/numbers' },
-  { label: 'config-ui - tabs', path: '/config-ui/tabs' },
-  { label: 'correct-answer-toggle', path: '/correct-answer-toggle' },
-  { label: 'editable-html', path: '/editable-html' },
-  { label: 'graphing', path: '/graphing' },
-  { label: 'charting', path: '/charting' },
-  { label: 'icons', path: '/icons' },
-  { label: 'math-evaluator', path: '/math-evaluator' },
-  { label: 'math-input', path: '/math-input' },
-  { label: 'render-ui', path: '/render-ui' },
-  { label: 'scoring-config', path: '/scoring-config' },
-  { label: 'text-select', path: '/text-select' },
-  { label: 'tools - ruler', path: '/tools/ruler' },
-  { label: 'tools - protractor', path: '/tools/protractor' },
-  { label: 'tools - rotatable', path: '/tools/rotatable' },
-  { label: 'drag', path: '/drag' }
-];
+import links from './links';
 
 function withRoot(Component) {
   class WithRoot extends React.Component {
