b29bc9a50123944921ee65f1a561c32c080b5455
Elod-Arpad Szopos <elodszopos@hotmail.com>
2019-02-12 13:28:22 +0200

    fix: remove unnecessary mathquill wrappers in favor of @pie-lib/math-input and small styling fixes

--- packages/demo/pages/math-toolbar.js
+++ packages/demo/pages/math-toolbar.js
@@ -5,8 +5,23 @@ import { MathToolbar } from '@pie-lib/math-toolbar';
 import withRoot from '../src/withRoot';
 
 export class Demo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mounted: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ mounted: true });
+  }
   render() {
-    return <MathToolbar latex={'\\frac{1}{2}'} />;
+    const { mounted } = this.state;
+    return mounted ? (
+      <MathToolbar latex={'\\frac{1}{2}'} />
+    ) : (
+      <div>loading</div>
+    );
   }
 }
 
