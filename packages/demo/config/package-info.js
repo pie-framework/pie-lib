062c4f8e579a3d3b6739627adc5742131f7ecbe4
ed eustace <ed.eustace@gmail.com>
2019-03-11 13:37:59 +0000

    return empty string if changelog not found

--- packages/demo/config/package-info.js
+++ packages/demo/config/package-info.js
@@ -4,11 +4,9 @@ const { resolve, join } = require('path');
 
 const safeRead = (dir, file) => {
   try {
-    console.log('[safeRead] dir: ', dir, file);
     return fs.readFileSync(join(dir, file), 'utf8');
   } catch (e) {
-    console.log('e:', e);
-    return undefined;
+    return '';
   }
 };
 
@@ -17,10 +15,7 @@ exports.load = () => {
 
   const filtered = packages.filter(pk => !pk.dir.endsWith('demo'));
 
-  console.log('files: ', filtered.length);
-
   return filtered.map(pk => {
-    console.log('read files for: ', pk.dir);
     const nextChangelog = safeRead(pk.dir, 'NEXT.CHANGELOG.md');
     const changelog = safeRead(pk.dir, 'CHANGELOG.md');
     return { ...pk, nextChangelog, changelog };
