78fe0a11eaaf23db7134e08d2b2ed2a2cd1b427b
Elod-Arpad Szopos <elodszopos@hotmail.com>
2019-04-10 10:22:39 +0300

    feat(math-evaluator): introduce partial rationalization, polynomial equation fixes and allowDecimals option

--- packages/math-evaluator/src/__tests__/index.test.js
+++ packages/math-evaluator/src/__tests__/index.test.js
@@ -2,111 +2,194 @@ import areValuesEqual from '../index';
 import _ from 'lodash';
 
 describe('math-evaluator', () => {
+  const assert = isEqual => label =>
+    function() {
+      const args = Array.from(arguments);
+      const pairs = _.chunk(args, 2);
 
-  const assert = (isEqual) => (label) => function () {
-    const args = Array.from(arguments);
-    const pairs = _.chunk(args, 2);
-
-    describe(label, () => {
-      pairs.forEach(([a, b]) => {
-        it(`${a} === ${b} ? ${isEqual}`, () => {
-          expect(areValuesEqual(a, b)).toBe(isEqual);
+      describe(label, () => {
+        pairs.forEach(([a, b]) => {
+          it(`${a} === ${b} ? ${isEqual}`, () => {
+            expect(areValuesEqual(a, b)).toBe(isEqual);
+          });
         });
       });
-    });
-  }
+    };
 
   const assertEqual = assert(true);
   const assertNotEqual = assert(false);
 
   assertNotEqual('evaluates simple expressions correctly')(
-    '0', 'x',
-    '3x', '4x',
+    '0',
+    'x',
+    '3x',
+    '4x'
   );
 
-  assertEqual('evaluates simple expressions correctly')(
-    'x', 'x',
-    '2x', '2x',
-  );
+  assertEqual('evaluates simple expressions correctly')('x', 'x', '2x', '2x');
 
   assertEqual('evaluates simple expressions correctly 2')(
-    '100', '100',
-    '50 + 50', '25 * 4',
-    '200 / 2', '20 * 5',
-    '2.5 * 40', '10 * 10',
+    '100',
+    '100',
+    '50 + 50',
+    '25 * 4',
+    '200 / 2',
+    '20 * 5',
+    '2.5 * 40',
+    '10 * 10'
   );
 
   assertNotEqual('evaluates simple expressions correctly 2')(
-    '100', '0',
-    '0', '50 * 2',
-    '44 + 57', '100',
-    '44 + 57', '50 * 3',
+    '100',
+    '0',
+    '0',
+    '50 * 2',
+    '44 + 57',
+    '100',
+    '44 + 57',
+    '50 * 3'
   );
 
   assertEqual('evaluates simple expressions correctly 3')(
-    '50 + 50', '100',
-    '100 / 2 + 50', '100',
-    '2 + 2', '4',
-    '1/2 + .5', '1',
+    '50 + 50',
+    '100',
+    '100 / 2 + 50',
+    '100',
+    '2 + 2',
+    '4',
+    '1/2 + .5',
+    '1'
   );
 
   assertNotEqual('evaluates simple expressions correctly 3')(
-    '50 + 51', '100',
-    '25 * 2 + 51', '100',
-    '1/2 + .5', '1.9',
-    '1/2 + .5', '1.1',
+    '50 + 51',
+    '100',
+    '25 * 2 + 51',
+    '100',
+    '1/2 + .5',
+    '1.9',
+    '1/2 + .5',
+    '1.1'
   );
 
   assertEqual('evaluates simple expressions correctly 4')(
-    '50 + 50', '100',
-    '100 / 2 + 50', '100',
-    '2 + 2', '4',
-    '1/2 + .5', '1',
-    '13/2', '13/2',
-    '26/4', '13/2',
-    '6 + 1/2', '13/2',
-    '6.5', '13/2',
+    '50 + 50',
+    '100',
+    '100 / 2 + 50',
+    '100',
+    '2 + 2',
+    '4',
+    '1/2 + .5',
+    '1',
+    '13/2',
+    '13/2',
+    '26/4',
+    '13/2',
+    '6 + 1/2',
+    '13/2',
+    '6.5',
+    '13/2'
   );
 
   assertNotEqual('evaluates simple expressions correctly 4')(
-    '50 + 51', '100',
-    '25 * 2 + 51', '100',
-    '1/2 + .5', '1.9',
-    '1/2 + .5', '1.1',
-    '14/2', '13/2',
-    '6.6', '13/2',
+    '50 + 51',
+    '100',
+    '25 * 2 + 51',
+    '100',
+    '1/2 + .5',
+    '1.9',
+    '1/2 + .5',
+    '1.1',
+    '14/2',
+    '13/2',
+    '6.6',
+    '13/2'
   );
 
   assertEqual('evaluates simple variable expressions correctly')(
-    'x', 'x',
-    'x + 0', 'x',
-    '(x-2) + 2', 'x',
-    '((x^2 + x) / x) - 1', 'x',
-    '2x/2', 'x',
-    '(x + 2)^2', '(x + 2)^2',
-    'x^2 + 4x + 4', '(x + 2)^2',
-    'x^2 + 4(x+1)', '(x + 2)^2',
-    'x^2 + 8 ((x+1) / 2)', '(x + 2)^2',
+    'x',
+    'x',
+    'x + 0',
+    'x',
+    '(x-2) + 2',
+    'x',
+    '((x^2 + x) / x) - 1',
+    'x',
+    '2x/2',
+    'x',
+    '(x + 2)^2',
+    '(x + 2)^2',
+    'x^2 + 4x + 4',
+    '(x + 2)^2',
+    'x^2 + 4(x+1)',
+    '(x + 2)^2',
+    'x^2 + 8 ((x+1) / 2)',
+    '(x + 2)^2'
   );
 
   assertNotEqual('evaluates simple variable expressions correctly')(
-    'y', 'x',
-    'x + 1', 'x',
-    'x^3 + 4x + 4', '(x + 2)^2',
-    'x^2 + 4(x+2)', '(x + 2)^2',
+    'y',
+    'x',
+    'x + 1',
+    'x',
+    'x^3 + 4x + 4',
+    '(x + 2)^2',
+    'x^2 + 4(x+2)',
+    '(x + 2)^2'
   );
 
   assertEqual('evaluates simple variable expressions correctly 2')(
-    '(2 + x)^2', '(x + 2)^2',
-    'x^2 + 4x + 4', '(x + 2)^2',
-    'x^2 + 4(x+1)', '(x + 2)^2',
-    'x^2 + 8 ((x+1) / 2)', '(x + 2)^2',
+    '(2 + x)^2',
+    '(x + 2)^2',
+    'x^2 + 4x + 4',
+    '(x + 2)^2',
+    'x^2 + 4(x+1)',
+    '(x + 2)^2',
+    'x^2 + 8 ((x+1) / 2)',
+    '(x + 2)^2'
+  );
+
+  assertEqual('evaluates expressions correctly with variable exponents')(
+    '(2 + x)^2x',
+    '(x + 2)^2x',
+    'y^(2 x)',
+    'y^(x+x)'
+  );
+
+  assertEqual(
+    'evaluates function expressions correctly with variable parameters'
+  )(
+    'sqrt(4x)',
+    'sqrt(2x+2x)',
+    'sqrt(x^2)',
+    'sqrt(((x+1)^2) - ((x+1)^2) + x^2)'
   );
 
   assertNotEqual('evaluates simple variable expressions correctly 2')(
-    'x^3 + 4x + 4', '(x + 2)^2',
-    'x^2 + 4(x+2)', '(x + 2)^2',
+    'x^3 + 4x + 4',
+    '(x + 2)^2',
+    'x^2 + 4(x+2)',
+    '(x + 2)^2'
   );
+
+  it('correctly consumes allowDecimals option', () => {
+    expect(areValuesEqual('123', '123', { allowDecimals: true })).toEqual(true);
+    expect(areValuesEqual('123', '123.0', { allowDecimals: true })).toEqual(
+      true
+    );
+    expect(areValuesEqual('123,0', '123.0', { allowDecimals: true })).toEqual(
+      true
+    );
+    expect(areValuesEqual('123,0', '123.0', { allowDecimals: false })).toEqual(
+      false
+    );
+    expect(areValuesEqual('123', '123.0', { allowDecimals: false })).toEqual(
+      false
+    );
+    expect(areValuesEqual('123', '123,0', { allowDecimals: false })).toEqual(
+      false
+    );
+  });
 
   it('evaluates simple trigo expressions correctly', () => {
     expect(areValuesEqual('sin(x)', 'sin(x)')).toEqual(true);
--- packages/math-evaluator/src/index.js
+++ packages/math-evaluator/src/index.js
@@ -1,33 +1,92 @@
 import mathjs from 'mathjs';
 import mathExpressions from 'math-expressions';
 
-function prepareExpression(string, isLatex) {
-  let returnValue = string ? string.trim() : '';
+const decimaCommalRegex = /,/g;
+const decimalRegex = /\.|,/g;
 
-  return isLatex
-    ? mathExpressions.fromLatex(`${returnValue}`).toString()
-    : mathExpressions.fromText(`${returnValue}`).toString();
+function rationalizeAllPossibleSubNodes(expression) {
+  const tree = mathjs.parse(expression);
+  const transformedTree = tree.transform(node => {
+    try {
+      const rationalizedNode = mathjs.rationalize(node);
+
+      return rationalizedNode;
+    } catch {
+      return node;
+    }
+  });
+
+  return transformedTree;
 }
 
-function canExpressionBeRationalized(expression) {
-  return !['sin', 'cos', 'tan', 'ctg'].some(trigFunc =>
-    `${expression}`.includes(trigFunc)
-  );
+function prepareExpression(string, isLatex) {
+  let returnValue = string ? string.trim() : '';
+  returnValue = returnValue.replace(decimaCommalRegex, '.');
+
+  returnValue = isLatex
+    ? mathExpressions.fromLatex(`${returnValue}`).toString()
+    : mathExpressions.fromText(`${returnValue}`).toString();
+
+  return rationalizeAllPossibleSubNodes(returnValue);
+}
+
+function shouldRationalizeEntireTree(tree) {
+  let shouldDoIt = true;
+
+  // we need to iterate the entire tree to check for some conditions that might make rationalization impossible
+  try {
+    tree.traverse(node => {
+      // if we have a variable as an exponent for power operation, we should not rationalize
+      // try to see if there are power operations with variable exponents
+      if (node.type === 'OperatorNode' && node.fn === 'pow') {
+        const exponent = node.args[1];
+
+        // try to see if it can be compiled and evaluated - if it's a non-numerical value, it'll throw an error
+        exponent.compile().eval();
+      }
+
+      // we cannot have variables for unresolved function calls either
+      if (node.type === 'FunctionNode') {
+        //try to see if the argument that the function is called with can be resolved as non-variable
+        const functionParameter = node.args[0];
+
+        // if it holds variables, this will throw an error
+        functionParameter.compile().eval();
+      }
+    });
+  } catch {
+    shouldDoIt = false;
+  }
+
+  return shouldDoIt;
+}
+
+function containsDecimal(expression = '') {
+  return expression.match(decimalRegex);
 }
 
 export default function areValuesEqual(valueOne, valueTwo, options = {}) {
   const {
+    // if explicitly set to false, having a decimal value in either side will result in a false equality
+    // regardless of mathematical correctness
+    allowDecimals,
     isLatex, // if the passed in values are latex, they need to be escaped
     inverse // returns inverse for the comparison result
   } = options;
 
+  if (allowDecimals === false) {
+    if (containsDecimal(valueOne) || containsDecimal(valueTwo)) {
+      return false;
+    }
+  }
+
   const preparedValueOne = prepareExpression(valueOne, isLatex);
   const preparedValueTwo = prepareExpression(valueTwo, isLatex);
 
-  let one = canExpressionBeRationalized(preparedValueOne)
+  let one = shouldRationalizeEntireTree(preparedValueOne)
     ? mathjs.rationalize(preparedValueOne)
     : preparedValueOne;
-  let two = canExpressionBeRationalized(preparedValueTwo)
+  let two = shouldRationalizeEntireTree(preparedValueTwo)
     ? mathjs.rationalize(preparedValueTwo)
     : preparedValueTwo;
 
