572ee77ceb7c3afe30aad2d3f2d8486efb0c7071
Elod-Arpad Szopos <elodszopos@hotmail.com>
2019-04-10 12:02:52 +0300

    feat: prettier and eslint git integration

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,12 @@
 // .prettierrc.js
 module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
   singleQuote: true,
   printWidth: 100
 };
