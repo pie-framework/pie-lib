cdd76498192f0fa45152edacd11e5ee7fdd97f19
Elod-Arpad Szopos <elodszopos@hotmail.com>
2019-05-08 13:49:15 +0300

    fix(math-input): fix trigo keypad buttons

--- packages/math-input/src/keys/trigonometry.js
+++ packages/math-input/src/keys/trigonometry.js
@@ -2,11 +2,14 @@ import { mkSet } from './utils';
 
 const set = mkSet('trigonometry');
 
-export const sin = set({ name: 'sin', label: 'sin', latex: '\\sin' });
+export const sin = set({ name: 'sin', label: 'sin', command: '\\sin', latex: '\\sin' });
 
-export const cos = set({ name: 'cos', label: 'cos', latex: '\\cos' });
+export const cos = set({ name: 'cos', label: 'cos', command: '\\cos', latex: '\\cos' });
 
-export const tan = set({ name: 'tan', label: 'tan', latex: '\\tan' });
-export const sec = set({ name: 'sec', label: 'sec', latex: '\\sec' });
-export const csc = set({ name: 'csc', label: 'csc', latex: '\\csc' });
-export const cot = set({ name: 'cot', label: 'cot', latex: '\\cot' });
+export const tan = set({ name: 'tan', label: 'tan', command: '\\tan', latex: '\\tan' });
+
+export const sec = set({ name: 'sec', label: 'sec', command: '\\sec', latex: '\\sec' });
+
+export const csc = set({ name: 'csc', label: 'csc', command: '\\csc', latex: '\\csc' });
+
+export const cot = set({ name: 'cot', label: 'cot', command: '\\cot', latex: '\\cot' });
