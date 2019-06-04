a03ce27258b2a68a16d5c7166d369d71f5c7840a
ed eustace <ed.eustace@gmail.com>
2019-06-04 21:33:18 +0100

    fix styles

--- packages/graphing/src/tools/line/component.jsx
+++ packages/graphing/src/tools/line/component.jsx
@@ -1,25 +1,52 @@
-import { lineToolComponent, lineBase } from '../shared/line-tools';
+import { lineToolComponent, lineBase, styles } from '../shared/line-tools';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { trig, types } from '@pie-lib/plot';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import { ArrowHead } from '../common/arrow-head';
 
-const ArrowedLine = props => {
-  const { graphProps, from, to, ...rest } = props;
+const ArrowedLine = withStyles(theme => ({
+  line: styles.line(theme)
+}))(props => {
+  const { className, classes, correctness, disabled, graphProps, from, to, ...rest } = props;
   const { scale } = graphProps;
   const [eFrom, eTo] = trig.edges(graphProps.domain, graphProps.range)(from, to);
-
+  const size = 20;
   return (
-    <line
-      stroke="cyan"
-      strokeWidth="7"
-      x1={scale.x(eFrom.x)}
-      y1={scale.y(eFrom.y)}
-      x2={scale.x(eTo.x)}
-      y2={scale.y(eTo.y)}
-      {...rest}
-    />
+    <g>
+      <defs>
+        <marker
+          id="arrow"
+          viewBox={`0 0 ${size} ${size}`}
+          refX={size / 2}
+          refY={size / 2}
+          markerWidth="10"
+          markerHeight="10"
+          orient="auto-start-reverse"
+          className={classes.arrow}
+        >
+          <ArrowHead />
+        </marker>
+      </defs>
+      <line
+        x1={scale.x(eFrom.x)}
+        y1={scale.y(eFrom.y)}
+        x2={scale.x(eTo.x)}
+        y2={scale.y(eTo.y)}
+        className={classNames(
+          classes.line,
+          disabled && classes.disabled,
+          classes[correctness],
+          className
+        )}
+        markerEnd="url(#arrow)"
+        markerStart="url(#arrow)"
+        {...rest}
+      />
+    </g>
   );
-};
+});
 
 ArrowedLine.propTypes = {
   graphProps: PropTypes.any,
--- packages/graphing/src/tools/segment/component.jsx
+++ packages/graphing/src/tools/segment/component.jsx
@@ -1,13 +1,16 @@
-import { lineToolComponent, lineBase } from '../shared/line-tools';
+import { lineToolComponent, lineBase, styles } from '../shared/line-tools';
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import { types } from '@pie-lib/plot';
+import { withStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
 
-const Line = props => {
-  const { graphProps, from, to, ...rest } = props;
+const Line = withStyles(theme => ({
+  line: styles.line(theme)
+}))(props => {
+  const { className, classes, correctness, disabled, graphProps, from, to, ...rest } = props;
   const { scale } = graphProps;
-  // const [eFrom, eTo] = trig.edges(graphProps.domain, graphProps.range)(from, to);
 
   return (
     <line
@@ -17,10 +20,16 @@ const Line = props => {
       y1={scale.y(from.y)}
       x2={scale.x(to.x)}
       y2={scale.y(to.y)}
+      className={classNames(
+        classes.line,
+        disabled && classes.disabled,
+        classes[correctness],
+        className
+      )}
       {...rest}
     />
   );
-};
+});
 
 Line.propTypes = {
   graphProps: PropTypes.any,
