f61d4c771ce07f8d74044388974c61ca9558fb02
lakatosandrei <lakatosandrei@gmail.com>
2019-08-14 07:28:33 +0300

    feat(componentize): removed unnecessary error thrown and variable

--- packages/mask-markup/src/componentize.js
+++ packages/mask-markup/src/componentize.js
@@ -1,13 +1,13 @@
 const REGEX = /\{\{(\d+)\}\}/g;
+
 export default (s, t) => {
-  const ids = [];
   if (!s) {
-    throw new Error('No markup');
+    return { markup: '' };
   }
 
   const markup = s.replace(REGEX, (match, g) => {
     return `<span data-component="${t}" data-id="${g}"></span>`;
   });
 
-  return { ids, markup };
+  return { markup };
 };
