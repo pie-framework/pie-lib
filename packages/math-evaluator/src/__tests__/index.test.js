e6c120bcd2715f6bb8ee687e646a8a6932601609
ed eustace <ed.eustace@gmail.com>
2019-11-08 16:44:37 +0000

    repro failure in test

--- packages/math-evaluator/src/__tests__/index.test.js
+++ packages/math-evaluator/src/__tests__/index.test.js
@@ -18,198 +18,204 @@ describe('math-evaluator', () => {
 
   const assertEqual = assert(true);
   const assertNotEqual = assert(false);
-
-  assertNotEqual('evaluates simple expressions correctly')('0', 'x', '3x', '4x');
-
-  assertEqual('evaluates simple expressions correctly')('x', 'x', '2x', '2x');
-
-  assertEqual('evaluates simple expressions correctly 2')(
-    '100',
-    '100',
-    '50 + 50',
-    '25 * 4',
-    '200 / 2',
-    '20 * 5',
-    '2.5 * 40',
-    '10 * 10'
-  );
-
-  assertNotEqual('evaluates simple expressions correctly 2')(
-    '100',
-    '0',
-    '0',
-    '50 * 2',
-    '44 + 57',
-    '100',
-    '44 + 57',
-    '50 * 3'
-  );
-
-  assertEqual('evaluates simple expressions correctly 3')(
-    '50 + 50',
-    '100',
-    '100 / 2 + 50',
-    '100',
-    '2 + 2',
-    '4',
-    '1/2 + .5',
-    '1'
-  );
-
-  assertNotEqual('evaluates simple expressions correctly 3')(
-    '50 + 51',
-    '100',
-    '25 * 2 + 51',
-    '100',
-    '1/2 + .5',
-    '1.9',
-    '1/2 + .5',
-    '1.1'
-  );
-
-  assertEqual('evaluates simple expressions correctly 4')(
-    '50 + 50',
-    '100',
-    '100 / 2 + 50',
-    '100',
-    '2 + 2',
-    '4',
-    '1/2 + .5',
-    '1',
-    '13/2',
-    '13/2',
-    '26/4',
-    '13/2',
-    '6 + 1/2',
-    '13/2',
-    '6.5',
-    '13/2'
-  );
-
-  assertNotEqual('evaluates simple expressions correctly 4')(
-    '50 + 51',
-    '100',
-    '25 * 2 + 51',
-    '100',
-    '1/2 + .5',
-    '1.9',
-    '1/2 + .5',
-    '1.1',
-    '14/2',
-    '13/2',
-    '6.6',
-    '13/2'
-  );
-
-  assertEqual('evaluates simple variable expressions correctly')(
-    'x',
-    'x',
-    'x + 0',
-    'x',
-    '(x-2) + 2',
-    'x',
-    '((x^2 + x) / x) - 1',
-    'x',
-    '2x/2',
-    'x',
-    '(x + 2)^2',
-    '(x + 2)^2',
-    'x^2 + 4x + 4',
-    '(x + 2)^2',
-    'x^2 + 4(x+1)',
-    '(x + 2)^2',
-    'x^2 + 8 ((x+1) / 2)',
-    '(x + 2)^2'
-  );
-
-  assertNotEqual('evaluates simple variable expressions correctly')(
-    'y',
-    'x',
-    'x + 1',
-    'x',
-    'x^3 + 4x + 4',
-    '(x + 2)^2',
-    'x^2 + 4(x+2)',
-    '(x + 2)^2'
-  );
-
-  assertEqual('evaluates simple variable expressions correctly 2')(
-    '(2 + x)^2',
-    '(x + 2)^2',
-    'x^2 + 4x + 4',
-    '(x + 2)^2',
-    'x^2 + 4(x+1)',
-    '(x + 2)^2',
-    'x^2 + 8 ((x+1) / 2)',
-    '(x + 2)^2'
-  );
-
-  assertEqual('evaluates expressions correctly with variable exponents')(
-    '(2 + x)^2x',
-    '(x + 2)^2x',
-    'y^(2 x)',
-    'y^(x+x)'
-  );
-
-  assertEqual('evaluates function expressions correctly with variable parameters')(
-    'sqrt(4x)',
-    'sqrt(2x+2x)',
-    'sqrt(x^2)',
-    'sqrt(((x+1)^2) - ((x+1)^2) + x^2)'
-  );
-
-  assertNotEqual('evaluates simple variable expressions correctly 2')(
-    'x^3 + 4x + 4',
-    '(x + 2)^2',
-    'x^2 + 4(x+2)',
-    '(x + 2)^2'
-  );
-
-  it('correctly consumes allowDecimals option', () => {
-    expect(areValuesEqual('123', '123', { allowDecimals: true })).toEqual(true);
-    expect(areValuesEqual('123', '123.0', { allowDecimals: true })).toEqual(true);
-    expect(areValuesEqual('123,0', '123.0', { allowDecimals: true })).toEqual(true);
-    expect(areValuesEqual('123,0', '123.0', { allowDecimals: false })).toEqual(false);
-    expect(areValuesEqual('123', '123.0', { allowDecimals: false })).toEqual(false);
-    expect(areValuesEqual('123', '123,0', { allowDecimals: false })).toEqual(false);
+  it.only('??', () => {
+    areValuesEqual('1', '\\odot', {
+      isLatex: true,
+      allowDecimals: true
+    });
   });
+  // assertEqual('custom latex')('1530', `\\odot`);
+  //   assertNotEqual('evaluates simple expressions correctly')('0', 'x', '3x', '4x');
 
-  it('correctly consumes allowDecimals option for complex examples too', () => {
-    expect(areValuesEqual('1500000', '1500000', { allowDecimals: true })).toEqual(true);
-    expect(areValuesEqual('1,500,000', '1,500,000', { allowDecimals: true })).toEqual(true);
-    expect(areValuesEqual('1500000', '1,500,000', { allowDecimals: true })).toEqual(true);
-    expect(areValuesEqual('1500000', '1,500,000.00', { allowDecimals: true })).toEqual(true);
-    expect(areValuesEqual('1500000', '1,500,000.0', { allowDecimals: true })).toEqual(true);
-    expect(areValuesEqual('1,500,000.0', '1,500,000.00', { allowDecimals: true })).toEqual(true);
-    expect(areValuesEqual('1500000', '1,500000', { allowDecimals: true })).toEqual(false);
-    expect(areValuesEqual('1500000', '1500,000', { allowDecimals: true })).toEqual(false);
-    expect(areValuesEqual('1500000', '1,500,000.01', { allowDecimals: true })).toEqual(false);
-    expect(areValuesEqual('1500000', '1,500,00,0', { allowDecimals: true })).toEqual(false);
-    expect(areValuesEqual('1500000', '1500,000', { allowDecimals: true })).toEqual(false);
-    expect(areValuesEqual('1500000', ',1500,000', { allowDecimals: true })).toEqual(false);
-  });
+  //   assertEqual('evaluates simple expressions correctly')('x', 'x', '2x', '2x');
 
-  it('evaluates simple trigo expressions correctly', () => {
-    expect(areValuesEqual('sin(x)', 'sin(x)')).toEqual(true);
-    expect(areValuesEqual('tan(x)', 'tan(x)')).toEqual(true);
-  });
+  //   assertEqual('evaluates simple expressions correctly 2')(
+  //     '100',
+  //     '100',
+  //     '50 + 50',
+  //     '25 * 4',
+  //     '200 / 2',
+  //     '20 * 5',
+  //     '2.5 * 40',
+  //     '10 * 10'
+  //   );
 
-  it('correctly creates math expressions 1', () => {
-    expect(
-      areValuesEqual(
-        'f^{-1}\\left(x\\right)=\\sqrt{x-1}+3',
-        'f^{-1}\\left(x\\right)=\\sqrt{x-1}+3',
-        { allowDecimals: true, isLatex: true }
-      )
-    ).toEqual(true);
-  });
+  //   assertNotEqual('evaluates simple expressions correctly 2')(
+  //     '100',
+  //     '0',
+  //     '0',
+  //     '50 * 2',
+  //     '44 + 57',
+  //     '100',
+  //     '44 + 57',
+  //     '50 * 3'
+  //   );
 
-  it.skip('correctly creates math expressions 2', () => {
-    expect(
-      areValuesEqual('72\\div12=6\\text{eggs}', '72\\div12=6\\text{eggs}', {
-        allowDecimals: true,
-        isLatex: true
-      })
-    ).toEqual(true);
-  });
+  //   assertEqual('evaluates simple expressions correctly 3')(
+  //     '50 + 50',
+  //     '100',
+  //     '100 / 2 + 50',
+  //     '100',
+  //     '2 + 2',
+  //     '4',
+  //     '1/2 + .5',
+  //     '1'
+  //   );
+
+  //   assertNotEqual('evaluates simple expressions correctly 3')(
+  //     '50 + 51',
+  //     '100',
+  //     '25 * 2 + 51',
+  //     '100',
+  //     '1/2 + .5',
+  //     '1.9',
+  //     '1/2 + .5',
+  //     '1.1'
+  //   );
+
+  //   assertEqual('evaluates simple expressions correctly 4')(
+  //     '50 + 50',
+  //     '100',
+  //     '100 / 2 + 50',
+  //     '100',
+  //     '2 + 2',
+  //     '4',
+  //     '1/2 + .5',
+  //     '1',
+  //     '13/2',
+  //     '13/2',
+  //     '26/4',
+  //     '13/2',
+  //     '6 + 1/2',
+  //     '13/2',
+  //     '6.5',
+  //     '13/2'
+  //   );
+
+  //   assertNotEqual('evaluates simple expressions correctly 4')(
+  //     '50 + 51',
+  //     '100',
+  //     '25 * 2 + 51',
+  //     '100',
+  //     '1/2 + .5',
+  //     '1.9',
+  //     '1/2 + .5',
+  //     '1.1',
+  //     '14/2',
+  //     '13/2',
+  //     '6.6',
+  //     '13/2'
+  //   );
+
+  //   assertEqual('evaluates simple variable expressions correctly')(
+  //     'x',
+  //     'x',
+  //     'x + 0',
+  //     'x',
+  //     '(x-2) + 2',
+  //     'x',
+  //     '((x^2 + x) / x) - 1',
+  //     'x',
+  //     '2x/2',
+  //     'x',
+  //     '(x + 2)^2',
+  //     '(x + 2)^2',
+  //     'x^2 + 4x + 4',
+  //     '(x + 2)^2',
+  //     'x^2 + 4(x+1)',
+  //     '(x + 2)^2',
+  //     'x^2 + 8 ((x+1) / 2)',
+  //     '(x + 2)^2'
+  //   );
+
+  //   assertNotEqual('evaluates simple variable expressions correctly')(
+  //     'y',
+  //     'x',
+  //     'x + 1',
+  //     'x',
+  //     'x^3 + 4x + 4',
+  //     '(x + 2)^2',
+  //     'x^2 + 4(x+2)',
+  //     '(x + 2)^2'
+  //   );
+
+  //   assertEqual('evaluates simple variable expressions correctly 2')(
+  //     '(2 + x)^2',
+  //     '(x + 2)^2',
+  //     'x^2 + 4x + 4',
+  //     '(x + 2)^2',
+  //     'x^2 + 4(x+1)',
+  //     '(x + 2)^2',
+  //     'x^2 + 8 ((x+1) / 2)',
+  //     '(x + 2)^2'
+  //   );
+
+  //   assertEqual('evaluates expressions correctly with variable exponents')(
+  //     '(2 + x)^2x',
+  //     '(x + 2)^2x',
+  //     'y^(2 x)',
+  //     'y^(x+x)'
+  //   );
+
+  //   assertEqual('evaluates function expressions correctly with variable parameters')(
+  //     'sqrt(4x)',
+  //     'sqrt(2x+2x)',
+  //     'sqrt(x^2)',
+  //     'sqrt(((x+1)^2) - ((x+1)^2) + x^2)'
+  //   );
+
+  //   assertNotEqual('evaluates simple variable expressions correctly 2')(
+  //     'x^3 + 4x + 4',
+  //     '(x + 2)^2',
+  //     'x^2 + 4(x+2)',
+  //     '(x + 2)^2'
+  //   );
+
+  //   it('correctly consumes allowDecimals option', () => {
+  //     expect(areValuesEqual('123', '123', { allowDecimals: true })).toEqual(true);
+  //     expect(areValuesEqual('123', '123.0', { allowDecimals: true })).toEqual(true);
+  //     expect(areValuesEqual('123,0', '123.0', { allowDecimals: true })).toEqual(true);
+  //     expect(areValuesEqual('123,0', '123.0', { allowDecimals: false })).toEqual(false);
+  //     expect(areValuesEqual('123', '123.0', { allowDecimals: false })).toEqual(false);
+  //     expect(areValuesEqual('123', '123,0', { allowDecimals: false })).toEqual(false);
+  //   });
+
+  //   it('correctly consumes allowDecimals option for complex examples too', () => {
+  //     expect(areValuesEqual('1500000', '1500000', { allowDecimals: true })).toEqual(true);
+  //     expect(areValuesEqual('1,500,000', '1,500,000', { allowDecimals: true })).toEqual(true);
+  //     expect(areValuesEqual('1500000', '1,500,000', { allowDecimals: true })).toEqual(true);
+  //     expect(areValuesEqual('1500000', '1,500,000.00', { allowDecimals: true })).toEqual(true);
+  //     expect(areValuesEqual('1500000', '1,500,000.0', { allowDecimals: true })).toEqual(true);
+  //     expect(areValuesEqual('1,500,000.0', '1,500,000.00', { allowDecimals: true })).toEqual(true);
+  //     expect(areValuesEqual('1500000', '1,500000', { allowDecimals: true })).toEqual(false);
+  //     expect(areValuesEqual('1500000', '1500,000', { allowDecimals: true })).toEqual(false);
+  //     expect(areValuesEqual('1500000', '1,500,000.01', { allowDecimals: true })).toEqual(false);
+  //     expect(areValuesEqual('1500000', '1,500,00,0', { allowDecimals: true })).toEqual(false);
+  //     expect(areValuesEqual('1500000', '1500,000', { allowDecimals: true })).toEqual(false);
+  //     expect(areValuesEqual('1500000', ',1500,000', { allowDecimals: true })).toEqual(false);
+  //   });
+
+  //   it('evaluates simple trigo expressions correctly', () => {
+  //     expect(areValuesEqual('sin(x)', 'sin(x)')).toEqual(true);
+  //     expect(areValuesEqual('tan(x)', 'tan(x)')).toEqual(true);
+  //   });
+
+  //   it('correctly creates math expressions 1', () => {
+  //     expect(
+  //       areValuesEqual(
+  //         'f^{-1}\\left(x\\right)=\\sqrt{x-1}+3',
+  //         'f^{-1}\\left(x\\right)=\\sqrt{x-1}+3',
+  //         { allowDecimals: true, isLatex: true }
+  //       )
+  //     ).toEqual(true);
+  //   });
+
+  //   it.skip('correctly creates math expressions 2', () => {
+  //     expect(
+  //       areValuesEqual('72\\div12=6\\text{eggs}', '72\\div12=6\\text{eggs}', {
+  //         allowDecimals: true,
+  //         isLatex: true
+  //       })
+  //     ).toEqual(true);
+  //   });
 });
