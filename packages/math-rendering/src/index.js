34a3d8df05f68587e966da85381bd93194555fd2
ed eustace <ed.eustace@gmail.com>
2019-12-12 20:57:54 +0000

    feat: remove HtmlAndMath
    BREAKING CHANGE: `HtmlAndMath` is now in `@pie-lib/render-ui`.

--- packages/math-rendering/src/index.js
+++ packages/math-rendering/src/index.js
@@ -1,5 +1,4 @@
 import renderMath from './render-math';
-import HtmlAndMath from './html-and-math';
 import { wrapMath, unWrapMath } from './normalization';
 
-export { renderMath, HtmlAndMath, wrapMath, unWrapMath };
+export { renderMath, wrapMath, unWrapMath };
