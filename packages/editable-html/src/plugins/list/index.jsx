223469ebdbd2374474c3d17e10425c13e7b5ea37
andreeapescar <andreeaflaviapescar@gmail.com>
2020-02-11 11:54:41 +0200

    Lint fix.

--- packages/editable-html/src/plugins/list/index.jsx
+++ packages/editable-html/src/plugins/list/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'; // eslint-disable-line
+import React from 'react';
 import PropTypes from 'prop-types';
 import EditList from 'slate-edit-list';
 import debug from 'debug';
