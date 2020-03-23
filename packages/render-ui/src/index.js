2e044491eee7f5dd9a0186facff3496e12ffd445
lakatosandrei <lakatosandrei@gmail.com>
2020-03-23 10:41:00 +0200

    feat(es6): updated index file and yarn

--- packages/render-ui/src/index.js
+++ packages/render-ui/src/index.js
@@ -5,4 +5,5 @@ import withUndoReset from './withUndoReset';
 import PreviewLayout from './preview-layout';
 import HtmlAndMath from './html-and-math';
 export InputContainer from './input-container';
+
 export { HtmlAndMath, indicators, withUndoReset, Feedback, PreviewLayout, Collapsible };
