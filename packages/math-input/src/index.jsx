10c40b1399e16a71f7eda321484d1d618d8378e2
ed eustace <ed.eustace@gmail.com>
2020-03-30 22:07:55 +0100

    fix: correct export syntax

--- packages/math-input/src/index.jsx
+++ packages/math-input/src/index.jsx
@@ -1,15 +1,17 @@
-export { keysForGrade } from './keys/grades';
+import { keysForGrade } from './keys/grades';
 
 const addLeftBracket = s => (s.indexOf('\\(') === 0 ? s : `\\(${s}`);
 const addRightBracket = s => (s.indexOf('\\)') === s.length - 2 ? s : `${s}\\)`);
 const rmLeftBracket = s => (s.indexOf('\\(') === 0 ? s.substring(2) : s);
 const rmRightBracket = s => (s.indexOf('\\)') === s.length - 2 ? s.substring(0, s.length - 2) : s);
 
-export const addBrackets = s => addRightBracket(addLeftBracket(s));
-export const removeBrackets = s => rmRightBracket(rmLeftBracket(s));
+const addBrackets = s => addRightBracket(addLeftBracket(s));
+const removeBrackets = s => rmRightBracket(rmLeftBracket(s));
 
-export * as keys from './keys';
+import * as keys from './keys';
 
-export HorizontalKeypad from './horizontal-keypad';
+import HorizontalKeypad from './horizontal-keypad';
 
-export * as mq from './mq';
+import * as mq from './mq';
+
+export { keysForGrade, addBrackets, removeBrackets, keys, HorizontalKeypad, mq };
