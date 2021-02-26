28bf2e25781610c21a65909837b25dbd4d3759c7
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2021-02-26 13:37:40 +0200

    update tests

--- packages/render-ui/src/__tests__/has-text.test.js
+++ packages/render-ui/src/__tests__/has-text.test.js
@@ -2,29 +2,20 @@ import * as React from 'react';
 import { hasText } from '../has-text';
 
 describe('has-text', () => {
-  it('div with text returns true', () => {
-    const input = '<div>Rationale</div>';
+  it.each`
+    input                     | expected
+    ${'<div>Rationale</div>'} | ${true}
+    ${'Rationale'}            | ${true}
+    ${'<div>hi</div>'}        | ${true}
+    ${'<div>hi'}              | ${true}
+    ${'<div></div>'}          | ${false}
+    ${'<div>  </div>'}        | ${false}
+    ${'<div><br />   </div>'} | ${false}
+    ${' '}                    | ${false}
+    ${null}                   | ${false}
+  `('$input -> $expected', ({ input, expected }) => {
+    const output = hasText(input);
 
-    expect(hasText(input)).toEqual(true);
-  });
-
-  it('text returns true', () => {
-    const input = 'Rationale';
-    expect(hasText(input)).toEqual(true);
-  });
-
-  it('empty div returns false', () => {
-    const input = '<div></div>';
-    expect(hasText(input)).toEqual(false);
-  });
-
-  it('empty string returns false', () => {
-    const input = '';
-    expect(hasText(input)).toEqual(false);
-  });
-
-  it('null returns false', () => {
-    const input = null;
-    expect(hasText(input)).toEqual(false);
+    expect(output).toEqual(expected);
   });
 });
