d4b018eff7b12251c2f73c1d2381dac0f11d2c39
Carla Costea <carlacostea@yahoo.com>
2022-04-28 11:22:26 +0300

    moove chart-type dropdown from pie elements to pie-lib, in charting config panel

---
+++ packages/charting/src/chart-type.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+
+const ChartType = withStyles(theme => ({
+  formControl: {
+    width: '100%'
+  },
+  chartType: {
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  },
+  chartTypeLabel: {
+    transform: 'translate(0px, -16px) scale(0.75) !important'
+  }
+}))(({ onChange, value, classes }) => {
+  return (
+    <div className={classes.chartType}>
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel htmlFor="type-helper" className={classes.chartTypeLabel}>
+          ChartType
+        </InputLabel>
+
+        <Select
+          value={value}
+          onChange={onChange}
+          input={<OutlinedInput name="type" id="type-helper" />}
+        >
+          <MenuItem value={'histogram'}>Histogram</MenuItem>
+          <MenuItem value={'bar'}>Bar</MenuItem>
+          <MenuItem value={'lineDot'}>Line Dot</MenuItem>
+          <MenuItem value={'lineCross'}>Line Cross</MenuItem>
+          <MenuItem value={'dotPlot'}>Dot Plot</MenuItem>
+          <MenuItem value={'linePlot'}>Line Plot</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+});
+
+export default ChartType;
--- packages/charting/src/config-chart.jsx
+++ packages/charting/src/config-chart.jsx
@@ -4,36 +4,14 @@ import { color } from '@pie-lib/render-ui';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-
-const GridConfig = ({ classes, disabled, labelInterval, gridInterval, onChange }) => {
-  return (
-    <div className={classes.columnView}>
-      <TextField
-        label={'Max Value'}
-        type={'number'}
-        value={maxValue}
-        variant={'outlined'}
-        disabled={disabled}
-        className={classes.textField}
-        onChange={v => onChange('max', v)}
-      />
-    </div>
-  );
-};
+import ChartType from './chart-type';
 
 const ConfigChart = props => {
   const { classes, model, onChange } = props;
-  const sizeProps = {
-    min: 2,
-    max: 10
-  };
 
   const { range } = model;
   const size = model.graph;
-  console.log(props, 'CHARTING PROPS');
-  console.log(size, 'grasizeph');
 
-  console.log(size.width, 'grapsizeh');
   const gridProps = { min: 2, max: 41 };
 
   const onSizeChanged = (key, e) => {
@@ -41,14 +19,13 @@ const ConfigChart = props => {
     const min = 50;
     const max = 700;
     const value = parseInt(e.target.value);
-    console.log(key, 'key');
-    console.log(size[key], 'GRsizeAPH KEY');
     const nextValue = value <= size[key] ? size[key] - step : size[key] + step;
 
     console.log(nextValue, 'nextValue');
 
     if (nextValue < min || nextValue > max) {
       return;
+      s;
     }
 
     const graph = { ...size, [key]: nextValue };
@@ -57,50 +34,32 @@ const ConfigChart = props => {
     onChange({ ...model, graph });
   };
 
-  const onDomainChanged = (key, e) => {
-    const { value } = e.target;
-    const parsedValue = key !== 'axisLabel' ? parseInt(value) : value;
+  const onRangeChanged = (key, e) => {};
 
-    if (!includeAxes && key === 'max' && (value < gridProps.min || value > gridProps.max)) {
-      return;
-    }
-
-    domain[key] = parsedValue;
-
-    onChange({ domain, range });
-  };
-
-  const onRangeChanged = (key, e) => {
-    const { value } = e.target;
-
-    if (!includeAxes && key === 'max' && (value < gridProps.min || value > gridProps.max)) {
-      return;
-    }
-
-    range[key] = key !== 'axisLabel' ? parseInt(value) : value;
-
-    onChange({ range });
-  };
+  const onChartTypeChange = chartType => onChange({ ...model, chartType });
 
   return (
     <div className={classes.wrapper}>
       <Typography variant={'subtitle1'}>Configure Chart</Typography>
 
       <div className={classes.content}>
-        <TextField
-          label={'Max Value'}
-          type={'number'}
-          value={range.max}
-          variant={'outlined'}
-          //disabled={disabled}
-          className={classes.textField}
-          onChange={v => onChange('max', v)}
-        />
+        <div className={classes.rowView}>
+          <ChartType value={model.chartType} onChange={e => onChartTypeChange(e.target.value)} />
+          <TextField
+            label={'Max Value'}
+            type={'number'}
+            value={range.max}
+            variant={'outlined'}
+            //disabled={disabled}
+            className={classes.textField}
+            onChange={v => onChange('max', v)}
+          />
+        </div>
         <div className={classes.rowView}>
           <TextField
             label={'Grid Interval'}
             type={'number'}
-            //  value={gridInterval}
+            //  value={}
             variant={'outlined'}
             //  disabled={disabled}
             className={classes.textField}
@@ -109,7 +68,7 @@ const ConfigChart = props => {
           <TextField
             label={'Label Interval'}
             type={'number'}
-            //   value={labelInterval}
+            //   value={}
             variant={'outlined'}
             //disabled={disabled}
             className={classes.textField}
@@ -137,7 +96,7 @@ const ConfigChart = props => {
               label={'Height'}
               type={'number'}
               value={size.height}
-              inputProps={sizeProps}
+              // inputProps={sizeProps}
               variant={'outlined'}
               className={classes.smallTextField}
               onChange={v => onSizeChanged('height', v)}
