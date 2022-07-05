5c493262ccf2ca8ba4c80af42e566b4e7a8c3571
Sergiu Lucaci <sergius.lucaci@gmail.com>
2022-07-05 16:33:17 +0300

    feat(math-toolbar): Make math-toolbar style inline with mathjax from Numeric to Grade 8HS PD-1521

--- packages/math-toolbar/src/editor-and-pad.jsx
+++ packages/math-toolbar/src/editor-and-pad.jsx
@@ -260,6 +260,7 @@ export class EditorAndPad extends React.Component {
         <hr className={classes.hr} />
         {shouldShowKeypad && (
           <HorizontalKeypad
+            className={classes.keyboard}
             layoutForKeyPad={layoutForKeyPad}
             additionalKeys={additionalKeys}
             mode={controlledKeypadMode ? this.state.equationEditor : keypadMode}
@@ -382,6 +383,43 @@ const styles = theme => ({
   },
   error: {
     border: '2px solid red'
+  },
+  keyboard: {
+    '& *': {
+      fontFamily: 'MJXZERO, MJXTEX-I !important',
+
+      '& .mq-math-mode .mq-empty': {
+        padding: '9px 1px !important'
+      },
+
+      '& .mq-longdiv-inner': {
+        marginTop: '-1px',
+        marginLeft: '5px !important;',
+
+        '& > .mq-empty': {
+          padding: '0 !important',
+          marginLeft: '0px !important',
+          marginTop: '2px'
+        }
+      },
+
+      '& .mq-math-mode .mq-longdiv': {
+        display: 'flex !important'
+      },
+
+      '& .mq-math-mode .mq-supsub': {
+        fontSize: '70.7% !important'
+      },
+
+      '& .mq-math-mode .mq-sqrt-stem': {
+        marginTop: '-5px',
+        paddingTop: '4px'
+      },
+
+      '& .mq-math-mode .mq-paren': {
+        verticalAlign: 'middle !important'
+      }
+    }
   }
 });
 
