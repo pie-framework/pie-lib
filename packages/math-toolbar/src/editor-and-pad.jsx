647904f3dc4f59b86064be1d7b9bbd2a4184f96b
Sergiu Lucaci <sergius.lucaci@gmail.com>
2022-07-18 14:35:22 +0300

    Revert "tweaks"
    This reverts commit 8a45559237aca9e263d9e7f75d798c23777aa75a.

--- packages/math-toolbar/src/editor-and-pad.jsx
+++ packages/math-toolbar/src/editor-and-pad.jsx
@@ -342,11 +342,9 @@ const styles = theme => ({
         marginTop: '-2px !important',
         paddingTop: '5px !important'
       },
-
       '& .mq-supsub ': {
         fontSize: '70.7%'
       },
-
       '& .mq-math-mode .mq-supsub.mq-sup-only': {
         verticalAlign: '-0.1em !important',
 
@@ -369,11 +367,11 @@ const styles = theme => ({
     marginTop: '5px',
     marginBottom: '5px',
     marginRight: '5px',
+    fontFamily: 'Roboto, Helvetica, Arial, sans-serif !important',
 
     '& label': {
       fontFamily: 'Roboto, Helvetica, Arial, sans-serif !important'
     },
-
     '& div': {
       fontFamily: 'Roboto, Helvetica, Arial, sans-serif !important'
     }
