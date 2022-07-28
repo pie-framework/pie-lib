6fadd35daa7c1fbf0bb162b7b39c6ee47c358cd4
Carla Costea <carlacostea@yahoo.com>
2022-07-28 12:32:06 +0300

    cleanup

--- packages/charting/src/chart.jsx
+++ packages/charting/src/chart.jsx
@@ -111,9 +111,9 @@ export class Chart extends React.Component {
     }
   };
 
-  addCategory = (chartType, range) => {
+  addCategory = range => {
     const { onDataChange, data, categoryDefaultLabel, defineChart } = this.props;
-    console.log(defineChart, 'defineChart');
+
     onDataChange([
       ...data,
       {
@@ -140,16 +140,7 @@ export class Chart extends React.Component {
   };
 
   render() {
-    const {
-      classes,
-      className,
-      domain,
-      range,
-      size,
-      title,
-      addCategoryEnabled,
-      theme
-    } = this.props;
+    const { classes, className, domain, range, size, title, addCategoryEnabled } = this.props;
     let { chartType } = this.props;
 
     const defineChart = this.props.defineChart || false;
@@ -200,7 +191,7 @@ export class Chart extends React.Component {
           <ToolMenu
             className={classes.toolMenu}
             disabled={!addCategoryEnabled}
-            addCategory={() => this.addCategory(chartType, correctValues.range)}
+            addCategory={() => this.addCategory(correctValues.range)}
           />
         </div>
         <Root title={title} classes={classes} rootRef={r => (this.rootNode = r)} {...rootCommon}>
