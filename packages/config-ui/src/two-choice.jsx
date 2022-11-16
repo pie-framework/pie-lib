63b65d57460b776bb4d70bb48043ad3a943ae70f
arimieandreea <arimie_andreea@yahoo.com>
2022-11-16 17:37:13 +0200

    feat(config-ui): update radio settings to have more options than only 2-PD-1980

--- packages/config-ui/src/two-choice.jsx
+++ packages/config-ui/src/two-choice.jsx
@@ -35,7 +35,6 @@ class RawNChoice extends React.Component {
 
   render() {
     const { header, className, classes, customLabel, opts, value, direction } = this.props;
-
     const preppedOpts = opts.map(o => {
       return typeof o === 'string' ? { label: o, value: o } : o;
     });
