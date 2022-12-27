66f1f7890dfa113d8e753fc0f89bd9b10d39ea7c
andreeapescar <andreeaflaviapescar@gmail.com>
2022-12-27 12:38:00 +0200

    feat: add maxWidth (overflow scroll) when there's no Settings Panel PD-2425

--- packages/config-ui/src/layout/layout-contents.jsx
+++ packages/config-ui/src/layout/layout-contents.jsx
@@ -94,7 +94,12 @@ class RawLayoutContents extends React.Component {
           </Tabs>
         )}
         {mode === 'tabbed' && !hasSettingsPanel && (
-          <div style={configuration?.maxWidth ? { maxWidth: configuration?.maxWidth } : {}}>{children}</div>
+          <div
+            className={classnames(configuration?.maxWidth && classes.contentContainerMaxWidth)}
+            style={configuration?.maxWidth ? { maxWidth: configuration?.maxWidth } : {}}
+          >
+            {children}
+          </div>
         )}
       </div>
     );
