6e45b130ad2a72f236d3c89269fdc180687864fe
Carla <carlacostea@yahoo.com>
2023-02-28 23:38:22 +0200

    fix(charting): fix style for chart boxes

--- packages/charting/src/chart.jsx
+++ packages/charting/src/chart.jsx
@@ -219,7 +219,7 @@ export class Chart extends React.Component {
     rootCommon.graphProps.size.height += top + increaseHeight;
 
     return (
-      <div className={classNames(classes.chart, className)}>
+      <div className={classNames(classes.chart, classes.chartBox, className)}>
         <div className={classes.controls}>
           <ToolMenu
             className={classes.toolMenu}
@@ -288,7 +288,6 @@ const styles = (theme) => ({
     transform: 'translate(60px, 35px)',
   },
   controls: {
-    width: 'inherit',
     display: 'flex',
     justifyContent: 'space-between',
     padding: theme.spacing.unit,
@@ -303,6 +302,9 @@ const styles = (theme) => ({
   },
   toolMenu: {
     minHeight: '36px',
+  },
+  chartBox: {
+    width: 'min-content',
   },
 });
 
