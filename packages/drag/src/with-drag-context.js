04534da486616ddbfd08794361c9bdadaa2d1655
carlacostea <carlacostea@yahoo.com>
2023-06-05 12:25:56 +0300

    fix(drag): provide drag support for multiple-devices PD-2722

--- packages/drag/src/with-drag-context.js
+++ packages/drag/src/with-drag-context.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { MultiBackend } from 'react-dnd-multi-backend';
+import MultiBackend from 'react-dnd-multi-backend';
 import { DndProvider } from 'react-dnd';
-import { HTML5toTouch } from 'rdndmb-html5-to-touch';
+import HTML5toTouch from 'react-dnd-multi-backend/dist/esm/HTML5toTouch';
 
 export default (Component) => (props) => (
   <DndProvider backend={MultiBackend} options={HTML5toTouch} debugMode="true">
