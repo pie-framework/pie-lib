4e4bab63fb69e53d4cdff303d5bfd4fc18a5c7a0
carlacostea <carlacostea@yahoo.com>
2023-07-10 14:19:24 +0300

    fix(drag-in-the-blank): add preview for draged items on touch backend, wip

--- packages/drag/src/with-drag-context.js
+++ packages/drag/src/with-drag-context.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MultiBackend, { TouchTransition } from 'react-dnd-multi-backend';
+import MultiBackend, { TouchTransition, Preview } from 'react-dnd-multi-backend';
 import { DndProvider } from 'react-dnd';
 import { TouchBackend } from 'react-dnd-touch-backend';
 import { HTML5Backend } from 'react-dnd-html5-backend';
@@ -12,15 +12,20 @@ const HTML5toTouch = {
     {
       backend: TouchBackend,
       options: { enableMouseEvents: true },
-      // preview: true,
+      preview: true,
       transition: TouchTransition,
       skipDispatchOnTransition: true,
     },
   ],
 };
 
+const PreviewComponent = ({ itemType, item, style }) => (
+  <div style={{ ...style }} dangerouslySetInnerHTML={{ __html: item.choice.value }} />
+);
+
 export default (Component) => (props) => (
   <DndProvider backend={MultiBackend} options={HTML5toTouch} context={window}>
     <Component {...props} />
+    <Preview generator={PreviewComponent} {...props} />
   </DndProvider>
 );
