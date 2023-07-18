48e7f83c3c3ee9a2875f752939d96d9871923590
carlacostea <carlacostea@yahoo.com>
2023-07-18 13:50:44 +0300

    (drag-in-the-blank): add preview on drag items for touch backend PD-2992

--- packages/drag/src/with-drag-context.js
+++ packages/drag/src/with-drag-context.js
@@ -1,66 +1,66 @@
 import React, { useEffect, useRef } from 'react';
-import MultiBackend, { TouchTransition, Preview } from 'react-dnd-multi-backend';
+import MultiBackend, { TouchTransition, usePreview } from 'react-dnd-multi-backend';
 import { PreviewPrompt } from '@pie-lib/render-ui';
 import { DndProvider } from 'react-dnd';
 import { TouchBackend } from 'react-dnd-touch-backend';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { renderMath } from '@pie-lib/math-rendering';
 
-const HTML5toTouch = {
-  backends: [
-    {
-      backend: HTML5Backend,
-    },
-    {
-      backend: TouchBackend,
-      options: { enableMouseEvents: true },
-      preview: true,
-      transition: TouchTransition,
-      skipDispatchOnTransition: true,
-    },
-  ],
+const backends = [
+  { backend: HTML5Backend },
+  {
+    backend: TouchBackend,
+    options: { enableMouseEvents: true },
+    preview: true,
+    transition: TouchTransition,
+    skipDispatchOnTransition: true,
+  },
+];
+
+const MaskBlankStyle = {
+  border: '1px solid black',
+  color: 'black',
+  minWidth: '90px',
+  minHeight: '32px',
+  height: 'auto',
+  maxWidth: '374px',
+  display: 'flex',
+  padding: '4px',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: '16px',
 };
 
-const PreviewComponent = ({ itemType, item, style }) => {
-  // Default style
-  let customStyle = { ...style };
+const PreviewComponent = () => {
+  let root = useRef(null);
+  const preview = usePreview();
 
-  // Additional style if itemType is 'blank'
-  if (itemType === 'MaskBlank') {
-    customStyle = {
-      ...customStyle,
-      border: '1px solid black',
-      color: 'black',
-      minWidth: '90px',
-      minHeight: '32px',
-      height: 'auto',
-      maxWidth: '374px',
-      display: 'flex',
-      padding: '4px',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius: '16px',
-    };
+  useEffect(() => {
+    if (preview?.display && root.current) {
+      renderMath(root.current);
+    }
+  }, [preview?.display, preview?.item?.choice?.value]);
+
+  if (!preview.display) {
+    return null;
   }
 
+  const { itemType, item, style } = preview;
+  const customStyle = {
+    ...style,
+    ...(itemType === 'MaskBlank' ? MaskBlankStyle : {}),
+  };
+
   return (
-    <div style={customStyle}>
+    <div ref={root} style={customStyle}>
       <PreviewPrompt className="label" prompt={item.choice.value} tagName="span" />
     </div>
   );
 };
 
-export default (Component) => (props) => {
-  const root = useRef(null);
-
-  useEffect(() => {
-    renderMath(root);
-  }, []);
-
-  return (
-    <DndProvider backend={MultiBackend} options={HTML5toTouch} context={window}>
-      <Component {...props} />
-      <Preview ref={root} generator={PreviewComponent} {...props} />
-    </DndProvider>
-  );
-};
+export default (Component) => (props) => (
+  <DndProvider backend={MultiBackend} options={{ backends }} context={window}>
+    <Component {...props} />
+    <PreviewComponent />
+  </DndProvider>
+);
