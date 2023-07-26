c88e1fd376b6f2c4f20c5312dd0d960a5165e48e
carlacostea <carlacostea@yahoo.com>
2023-07-26 09:38:58 +0300

    (categorize): Add and enhance preview component for categorize drag functionality on touch devices PD-3049

--- packages/drag/src/preview-component.jsx
+++ packages/drag/src/preview-component.jsx
@@ -4,31 +4,39 @@ import { PreviewPrompt } from '@pie-lib/render-ui';
 import { renderMath } from '@pie-lib/math-rendering';
 import { color } from '@pie-lib/render-ui';
 
-const MaskBlankStyle = {
-  border: '1px solid black',
-  color: 'black',
-  minWidth: '90px',
-  minHeight: '32px',
-  height: 'auto',
-  maxWidth: '374px',
-  display: 'flex',
-  padding: '4px',
-  alignItems: 'center',
-  justifyContent: 'center',
-  borderRadius: '16px',
-};
-
-const ICAStyle = {
-  backgroundColor: color.background(),
-  border: `1px solid ${color.text()}`,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  minHeight: '28px',
-  padding: '0 3px',
-  marginLeft: 2,
-  marginTop: 2,
-  width: 'fit-content',
+const styles = {
+  maskBlank: {
+    border: '1px solid black',
+    color: 'black',
+    minWidth: '90px',
+    minHeight: '32px',
+    height: 'auto',
+    maxWidth: '374px',
+    display: 'flex',
+    padding: '4px',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '16px',
+  },
+  ica: {
+    backgroundColor: color.background(),
+    border: `1px solid ${color.text()}`,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '28px',
+    padding: '0 3px',
+    marginLeft: 2,
+    marginTop: 2,
+    width: 'fit-content',
+  },
+  categorize: {
+    color: color.text(),
+    backgroundColor: color.background(),
+    padding: '16px',
+    borderRadius: '4px',
+    border: '1px solid',
+  },
 };
 
 const getPrompt = (itemType, item) => {
@@ -40,7 +48,10 @@ const getPrompt = (itemType, item) => {
     case 'react-dnd-response':
       return item?.value;
     default:
-      return undefined;
+  }
+
+  if (itemType && item?.itemType === 'categorize') {
+    return item?.value;
   }
 };
 
@@ -62,9 +73,10 @@ const PreviewComponent = () => {
 
   const customStyle = {
     ...style,
-    ...(itemType === 'MaskBlank' ? MaskBlankStyle : {}),
+    ...(itemType === 'MaskBlank' ? styles.maskBlank : {}),
+    ...(item?.itemType === 'categorize' ? styles.categorize : {}),
     // TODO: In the image-cloze-association component, there's a noticeable delay in the image rendering process. This results in a brief display of an empty image placeholder before the actual image appears after a few seconds. This issue also impacts the correct rendering of the preview feature, thereby negatively affecting the user experience. This needs to be addressed promptly.
-    //...(itemType === 'react-dnd-response' ? ICAStyle : {}),
+    //...(itemType === 'react-dnd-response' ? styles.ica : {}),
   };
 
   const prompt = getPrompt(itemType, item);
