fd6ed48070979cd34bc64403228b728394a8afed
lakatosandrei <lakatosandrei@gmail.com>
2023-09-02 11:56:36 +0300

    fix: made sure multiple nodes are wrapped in a block, in order for slate to handle it properly PD-3051

--- packages/editable-html/src/serialization.jsx
+++ packages/editable-html/src/serialization.jsx
@@ -366,9 +366,45 @@ serializer.deserialize = function deserialize(html) {
   return null;
 };
 
+const reduceMultipleBrs = (markup) => {
+  try {
+    return markup.replace(/(<br\s*\/?>){3,}/gi, '<br>');
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log("Couldn't remove <br/> tags: ", e);
+  }
+
+  return markup;
+};
+
+const reduceRedundantNewLineCharacters = (markup) => {
+  try {
+    return markup.replace(/\n/gi, '');
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log("Couldn't remove <br/> tags: ", e);
+  }
+
+  return markup;
+};
+
+const wrapHtmlProperly = (markup) => {
+  const el = document.createElement('div');
+
+  el.innerHTML = markup;
+
+  if (el.children.length > 1) {
+    // this means we have multiple nodes that are not wrapped, so we wrap them so slate
+    // doesn't go crazy
+    return el.outerHTML;
+  }
+
+  return el.innerHTML;
+};
+
 export const htmlToValue = (html) => {
   try {
-    return serializer.deserialize(html);
+    return serializer.deserialize(wrapHtmlProperly(reduceRedundantNewLineCharacters(reduceMultipleBrs(html))));
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log("Couldn't parse html: ", e);
