b9b912913074a67451f83a80739e85b856320082
andreeapescar <andreeaflaviapescar@gmail.com>
2023-10-23 17:31:19 +0300

    chore: add source code

---
+++ packages/chart-toolbox/src/code/rubric/index.js
@@ -0,0 +1,8 @@
+import Authoring from './authoring';
+
+const RUBRIC_TYPES = {
+  SIMPLE_RUBRIC: 'simpleRubric',
+  MULTI_TRAIT_RUBRIC: 'multiTraitRubric',
+};
+
+export { Authoring, RUBRIC_TYPES };
