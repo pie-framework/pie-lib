e5e03fd3975623a7fdd5c0b9b4c88ae4b0b118d6
andreeapescar <andreeaflaviapescar@gmail.com>
2023-10-30 11:50:52 +0200

    fix: export necessary values

--- packages/pie-toolbox/src/editable-html.js
+++ packages/pie-toolbox/src/editable-html.js
@@ -1,3 +1,4 @@
-import EditableHtml from "./code/editable-html";
+import EditableHtml, { htmlToValue, valueToHtml, Editor, DEFAULT_PLUGINS, ALL_PLUGINS } from './code/editable-html';
 
+export { htmlToValue, valueToHtml, Editor, DEFAULT_PLUGINS, ALL_PLUGINS };
 export default EditableHtml;
