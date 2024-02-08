6b31ac697fc90ad43a80d733adf49574c6f5b4d7
carlacostea <carlacostea@yahoo.com>
2024-02-08 11:30:11 +0200

    feat(math-rendering): Ensure usage of the correctly instantiated MathJax and incorporate rendering options

--- packages/pie-toolbox/src/code/math-rendering/render-math.js
+++ packages/pie-toolbox/src/code/math-rendering/render-math.js
@@ -2,11 +2,42 @@ import { initializeMathJax } from './mathjax-script';
 import { wrapMath, unWrapMath } from './normalization';
 
 import { SerializedMmlVisitor } from 'mathjax-full/js/core/MmlTree/SerializedMmlVisitor';
+
 const visitor = new SerializedMmlVisitor();
 const toMMl = (node) => visitor.visitTree(node);
 
 const NEWLINE_BLOCK_REGEX = /\\embed\{newLine\}\[\]/g;
 const NEWLINE_LATEX = '\\newline ';
+
+export const getGlobal = () => {
+  // TODO does it make sense to use version?
+  // const key = `${pkg.name}@${pkg.version.split('.')[0]}`;
+  // It looks like Ed made this change when he switched from mathjax3 to mathjax-full
+  // I think it was supposed to make sure version 1 (using mathjax3) is not used
+  // in combination with version 2 (using mathjax-full)
+
+  // TODO higher level wrappers use this instance of math-rendering, and if 2 different instances are used, math rendering is not working
+  //  so I will hardcode this for now until a better solution is found
+  const key = '@pie-lib/math-rendering-accessible@1';
+
+  if (typeof window !== 'undefined') {
+    if (!window[key]) {
+      window[key] = {};
+    }
+    return window[key];
+  } else {
+    return {};
+  }
+};
+
+/** Add temporary support for a global singleDollar override
+ *  <code>
+ *   // This will enable single dollar rendering
+ *   window.pie = window.pie || {};
+ *   window.pie.mathRendering =  {useSingleDollar: true };
+ *  </code>
+ */
+const defaultOpts = () => getGlobal().opts || {};
 
 export const fixMathElement = (element) => {
   if (element.dataset.mathHandled) {
@@ -39,13 +70,9 @@ const adjustMathMLStyle = (el = document) => {
   nodes.forEach((node) => node.setAttribute('displaystyle', 'true'));
 };
 
-const preprocessLatex = (latex) => {
-  // Replace occurrences of \longdiv with an alternative expression
-  return latex.replace(/\\longdiv/g, '\\enclose{longdiv}');
-};
-
-
-const renderMath = (el, renderOpts, callback) => {
+const renderMath = (el, renderOpts) => {
+  renderOpts = renderOpts || defaultOpts();
+  
   const isString = typeof el === 'string';
   let executeOn = document.body;
 
@@ -63,70 +90,54 @@ const renderMath = (el, renderOpts, callback) => {
 
   if (!window.MathJax && !window.mathjaxLoadedP) {
     console.log('Initializing MathJax...');
-    initializeMathJax();
+    initializeMathJax(renderOpts);
   }
-  
+
   if (isString && window.MathJax && window.mathjaxLoadedP) {
     try {
-    MathJax.texReset();
-    MathJax.typesetClear();
-    window.MathJax.typeset([executeOn]);
-    const updatedDocument = window.MathJax.startup.document;
-    const list = updatedDocument.math.list;
-    const item = list.next;
-    const mathMl = toMMl(item.data.root);
+      MathJax.texReset();
+      MathJax.typesetClear();
+      window.MathJax.typeset([executeOn]);
+      const updatedDocument = window.MathJax.startup.document;
+      const list = updatedDocument.math.list;
+      const item = list.next;
+      const mathMl = toMMl(item.data.root);
 
+      const parsedMathMl = mathMl.replaceAll('\n', '');
 
-    const parsedMathMl = mathMl.replaceAll('\n', '');
+      console.log(parsedMathMl, 'parsedMathMl');
 
-    console.log(parsedMathMl, 'parsedMathMl');
-
-    return parsedMathMl;
-  } catch (error) {
-    console.error('Error rendering math:', error.message);
-   
-  }
-  // window.mathjaxLoadedP.then(() => {
-  //   try {
-  //     window.MathJax.texReset();
-  //     window.MathJax.typesetClear();
-  //     window.MathJax.typesetPromise([executeOn]).then(() => {
-  //       const updatedDocument = window.MathJax.startup.document;
-  //       const list = updatedDocument.math.list;
-  //       const item = list.next;
-  //       const mathMl = toMMl(item.data.root);
-  //       const parsedMathMl = mathMl.replaceAll('\n', '');
-  //       callback(null, parsedMathMl); // Call callback with the result
-  //     }).catch((error) => {
-  //       callback(error, null); // Call callback with the error
-  //     });
-  //   } catch (error) {
-  //     callback(error, null); // Call callback with the error
-  //   }
-  // });
+      return parsedMathMl;
+    } catch (error) {
+      console.error('Error rendering math:', error.message);
+    }
   }
 
   if (window.mathjaxLoadedP) {
     window.mathjaxLoadedP
       .then(() => {
         console.log('MathJax is ready, typesetting element');
-        if (window.MathJax) {
+
+        const mathJaxInstance = getGlobal().instance;
+        console.log(mathJaxInstance, 'mathJaxInstance');
+        if (mathJaxInstance) {
           // Reset and clear typesetting before processing the new content
 
           //  Reset the tex labels (and automatic equation number).
-          window.MathJax.texReset();
+          mathJaxInstance.texReset();
 
           //  Reset the typesetting system (font caches, etc.)
-          window.MathJax.typesetClear();
+          mathJaxInstance.typesetClear();
 
           // Use typesetPromise for asynchronous typesetting
           // Using MathJax.typesetPromise() for asynchronous typesetting to handle situations where additional code needs to be loaded (e.g., for certain TeX commands or characters).
           // This ensures typesetting waits for any needed resources to load and complete processing, unlike the synchronous MathJax.typeset() which can't handle such dynamic loading.
-          window.MathJax.typesetPromise([executeOn])
+          mathJaxInstance
+            .typesetPromise([executeOn])
             .then(() => {
               //console.log('Element after typesetting:', el.innerHTML);
               try {
-                const updatedDocument = window.MathJax.startup.document;
+                const updatedDocument = mathJaxInstance.startup.document;
                 const list = updatedDocument.math.list;
 
                 for (let item = list.next; typeof item.data !== 'symbol'; item = item.next) {
@@ -141,7 +152,7 @@ const renderMath = (el, renderOpts, callback) => {
               }
 
               // Clearing the document if needed
-              window.MathJax.startup.document.clear();
+              mathJaxInstance.startup.document.clear();
             })
             .catch((error) => {
               //  If there was an internal error, put the message into the output instead
