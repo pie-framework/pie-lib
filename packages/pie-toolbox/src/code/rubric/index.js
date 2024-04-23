5efea82535feeace71288a3c9040acdf09082234
Diana Gociu <diana.gociu@yahoo.com>
2024-04-23 11:09:12 +0200

    refactor: removed part of rubric code PD-3672
    BREAKING CHANGE: Remove unnecessary code regarding rubric after moving it to pie-elements

--- packages/pie-toolbox/src/code/rubric/index.js
+++
@@ -1,9 +0,0 @@
-import Authoring from './authoring';
-
-const RUBRIC_TYPES = {
-  SIMPLE_RUBRIC: 'simpleRubric',
-  MULTI_TRAIT_RUBRIC: 'multiTraitRubric',
-  RUBRICLESS: 'rubricless',
-};
-
-export { Authoring, RUBRIC_TYPES };
