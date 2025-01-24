e8eacd745fdfa22fb0b0e2241f8c92c292e991c5
Diana Gociu <diana.gociu@yahoo.com>
2025-01-24 13:40:27 +0200

    fix(math-templated): fix math rendering in math templated response areas PD-4589

--- packages/pie-toolbox/src/code/editable-html/plugins/respArea/math-templated/index.jsx
+++ packages/pie-toolbox/src/code/editable-html/plugins/respArea/math-templated/index.jsx
@@ -54,6 +54,51 @@ const styles = (theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'var(--pie-background, rgba(255, 255, 255, 0))',
+    '& > .mq-math-mode sup.mq-nthroot': {
+      fontSize: '70% !important',
+      verticalAlign: '1em !important',
+    },
+    '& > .mq-math-mode .mq-sqrt-stem': {
+      borderTop: '0.07em solid',
+      marginLeft: '-1.5px',
+      marginTop: '-2px !important',
+      paddingTop: '5px !important',
+    },
+    '& .mq-overarrow-inner': {
+      paddingTop: '0 !important',
+      border: 'none !important',
+    },
+    '& .mq-overarrow.mq-arrow-both': {
+      marginTop: '0px',
+      minWidth: '1.23em',
+      '& *': {
+        lineHeight: '1 !important',
+      },
+      '&:before': {
+        top: '-0.4em',
+        left: '-1px',
+      },
+      '&:after': {
+        top: '0px !important',
+        position: 'absolute !important',
+        right: '-2px',
+      },
+      '&.mq-empty:after': {
+        top: '-0.45em',
+      },
+    },
+    '& .mq-overarrow.mq-arrow-right': {
+      '&:before': {
+        top: '-0.4em',
+        right: '-1px',
+      },
+    },
+    '& .mq-overarrow-inner-right': {
+      display: 'none !important',
+    },
+    '& .mq-overarrow-inner-left': {
+      display: 'none !important',
+    },
   },
 });
 
