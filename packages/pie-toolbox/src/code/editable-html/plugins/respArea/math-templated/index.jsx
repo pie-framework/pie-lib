57571461f21d928b5201eb524ea04fa6b9e8dae4
Diana Gociu <diana.gociu@yahoo.com>
2025-01-27 10:52:49 +0200

    fix(math-templated): code formatting PD-4589

--- packages/pie-toolbox/src/code/editable-html/plugins/respArea/math-templated/index.jsx
+++ packages/pie-toolbox/src/code/editable-html/plugins/respArea/math-templated/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-
 import { mq } from '../../../../math-input';
 
 const MathTemplated = ({ attributes, value, classes, keyToDisplay }) => (
