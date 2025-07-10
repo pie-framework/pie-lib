0edca75bb2fd4470e7a6d4ac8d8f33762a23d6a6
arimieandreea <arimie_andreea@yahoo.com>
2025-07-10 11:43:20 +0300

    feat(charting): create KeyLegend component for charting PD-5083

--- packages/pie-toolbox/src/charting.js
+++ packages/pie-toolbox/src/charting.js
@@ -2,5 +2,6 @@ import Chart from './code/charting/chart';
 import chartTypes from './code/charting/chart-types';
 import ConfigureChartPanel from './code/charting/chart-setup';
 import ChartType from './code/charting/chart-type';
+import { KeyLegend } from './code/charting';
 
-export { Chart, chartTypes, ChartType, ConfigureChartPanel };
+export { Chart, chartTypes, ChartType, ConfigureChartPanel, KeyLegend };
