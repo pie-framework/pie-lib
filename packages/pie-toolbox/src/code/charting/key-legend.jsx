a7a7aa7065692ecf6284bad4569d44e4416a8f7f
arimieandreea <arimie_andreea@yahoo.com>
2025-07-16 13:05:21 +0300

    feat(charting): use material-ui icons PD-5083

--- packages/pie-toolbox/src/code/charting/key-legend.jsx
+++ packages/pie-toolbox/src/code/charting/key-legend.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Check from '@material-ui/icons/Check';
+import Close from '@material-ui/icons/Close';
 import { color } from '../render-ui';
-import Icons from './key-legend-icons';
 import Translator from '../translator';
 
 const styles = (theme) => ({
@@ -27,6 +28,23 @@ const styles = (theme) => ({
   smallText: {
     marginLeft: '2px',
   },
+  correctIcon: {
+    backgroundColor: color.correct(),
+    borderRadius: theme.spacing.unit * 2,
+    color: color.defaults.WHITE,
+  },
+  incorrectIcon: {
+    backgroundColor: color.incorrectWithIcon(),
+    borderRadius: theme.spacing.unit * 2,
+    color: color.defaults.WHITE,
+  },
+
+  lastRow: {
+    marginLeft: '3px',
+    display: 'flex',
+    alignItems: 'center',
+    gap: '12px',
+  },
 });
 
 const { translator } = Translator;
@@ -35,15 +53,15 @@ const KeyLegend = ({ classes, language }) => (
   <div className={classes.container}>
     <div className={classes.title}>Key</div>
     <div className={classes.row}>
-      <Icons.IncorrectAnswerIcon />
+      <Close className={classes.incorrectIcon} />
       <div className={classes.text}>{translator.t('charting.keyLegend.incorrectAnswer', { lng: language })}</div>
     </div>
     <div className={classes.row}>
-      <Icons.CorrectAnswerIcon />
+      <Check className={classes.correctIcon} />
       <div className={classes.text}>{translator.t('charting.keyLegend.correctAnswer', { lng: language })}</div>
     </div>
-    <div className={classes.row}>
-      <Icons.AnswerKeyCorrectIcon />
+    <div className={classes.lastRow}>
+      <Check className={classes.correctIcon} fontSize={'small'} />
       <div className={classes.smallText}>{translator.t('charting.keyLegend.correctKeyAnswer', { lng: language })}</div>
     </div>
   </div>
