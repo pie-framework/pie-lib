7ca6f32cf8bbd7b5d79479d80b8d2edfe86f2fb3
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2025-10-17 15:50:23 +0300

    feat: packages/scoring-config mui update PD-5268

--- packages/scoring-config/src/index.jsx
+++ packages/scoring-config/src/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Checkbox from '@material-ui/core/Checkbox';
-import { withStyles } from '@material-ui/core/styles';
+import Typography from '@mui/material/Typography';
+import Checkbox from '@mui/material/Checkbox';
+import { styled } from '@mui/material/styles';
 import PropTypes from 'prop-types';
+
+const ScoringConfigContainer = styled('div')(({ theme }) => ({
+  paddingTop: theme.spacing(1),
+}));
 
 export class PartialScoringConfig extends React.Component {
   static propTypes = {
     label: PropTypes.string,
     partialScoring: PropTypes.bool,
-    classes: PropTypes.object.isRequired,
     onChange: PropTypes.func,
   };
 
@@ -35,23 +38,19 @@ export class PartialScoringConfig extends React.Component {
   };
 
   render() {
-    const { classes, label } = this.props;
+    const { label } = this.props;
     const { checked } = this.state;
     const textLabel = label || 'Each correct response is worth 1/X where X is the number of correct answer selections.';
 
     return (
-      <div className={classes.scoringConfig}>
-        <Typography type="subheading">Partial Scoring Rules</Typography>
+      <ScoringConfigContainer>
+        <Typography variant="h6">Partial Scoring Rules</Typography>
         <br />
         <Typography>{textLabel}</Typography>
         <Checkbox checked={checked} onChange={this.onCheckboxChanged} label={''} />
-      </div>
+      </ScoringConfigContainer>
     );
   }
 }
 
-export default withStyles((theme) => ({
-  scoringConfig: {
-    paddingTop: theme.spacing.unit,
-  },
-}))(PartialScoringConfig);
+export default PartialScoringConfig;
