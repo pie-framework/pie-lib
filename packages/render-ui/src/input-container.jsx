80128173dc596181de296633e9413bba8b79db81
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2025-11-05 15:50:13 +0200

    fix: label positioning in input render-ui

--- packages/render-ui/src/input-container.jsx
+++ packages/render-ui/src/input-container.jsx
@@ -1,38 +1,50 @@
 import InputLabel from '@mui/material/InputLabel';
 import PropTypes from 'prop-types';
 import React from 'react';
-import classNames from 'classnames';
 import { styled } from '@mui/material/styles';
 import FormControl from '@mui/material/FormControl';
 
 const StyledFormControl = styled(FormControl)(({ theme }) => ({
-  marginLeft: 0,
-  marginRight: 0,
-  paddingBottom: theme.spacing(1),
+  margin: 0,
+  padding: 0,
   flex: '1 0 auto',
   minWidth: theme.spacing(4),
 }));
 
-const StyledInputLabel = styled(InputLabel)({
-  fontSize: 'inherit',
+const StyledInputLabel = styled(InputLabel)(() => ({
+ fontSize: 'inherit',
   whiteSpace: 'nowrap',
-});
+  margin: 0,
+  padding: 0,
+  alignSelf: 'flex-start',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  transformOrigin: 'top left',
+  pointerEvents: 'none',
+  // override MUI's default transform styles
+  '&.MuiInputLabel-shrink': {
+    transform: 'scale(0.75) translate(0, -0.75em)',
+  },
+  '&:not(.MuiInputLabel-shrink)': {
+    transform: 'translate(0, 0)',
+  },
+}));
 
-const InputContainer = (props) => {
-  const { label, className, children } = props;
-
-  return (
-    <StyledFormControl className={className}>
-      <StyledInputLabel shrink={true}>{label}</StyledInputLabel>
-      {children}
-    </StyledFormControl>
-  );
-};
+const InputContainer = ({ label, className, children }) => (
+  <StyledFormControl className={className}>
+    <StyledInputLabel shrink>{label}</StyledInputLabel>
+    {children}
+  </StyledFormControl>
+);
 
 InputContainer.propTypes = {
   label: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
   className: PropTypes.string,
-  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
 };
 
 export default InputContainer;
