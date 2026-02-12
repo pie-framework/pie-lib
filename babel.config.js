eb00fd518513192529253de6785dc81b659e49a8
Andrei Miron <andrei.miron@mcro-e.com>
2026-02-12 17:12:00 +0200

    configure babel to not inject polyfills

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,17 @@ module.exports = (api) => {
   const isTest = api.env('test');
 
   return {
-    presets: ['@babel/preset-env', '@babel/preset-react'
-    ],
+    presets: ['@babel/preset-env', '@babel/preset-react'],
     plugins: [
       '@babel/plugin-transform-class-properties',
       '@babel/plugin-proposal-export-default-from',
       '@babel/plugin-transform-export-namespace-from',
-      '@babel/plugin-transform-runtime',
+      [
+        '@babel/plugin-transform-runtime',
+        {
+          corejs: false,
+        },
+      ],
     ],
     // Don't ignore node_modules in test mode so ES modules can be transformed
     ignore: isTest ? ['packages/**/lib'] : ['node_modules', 'packages/**/lib'],
