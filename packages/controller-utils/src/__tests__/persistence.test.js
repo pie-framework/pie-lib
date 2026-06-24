dabe80923567799b4e81ca120426eae248bb9497
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2026-06-24 14:03:37 +0300

    fix(lockChoices): update logic to ensure instructor role does not lock choice order PIE-714

--- packages/controller-utils/src/__tests__/persistence.test.js
+++ packages/controller-utils/src/__tests__/persistence.test.js
@@ -96,9 +96,9 @@ describe('lockChoices', () => {
     ${false}     | ${undefined}       | ${undefined}                | ${false}
     ${undefined} | ${session()}       | ${env(false)}               | ${false}
     ${undefined} | ${session()}       | ${env(undefined)}           | ${false}
-    ${false}     | ${session()}       | ${env(false, 'instructor')} | ${true}
-    ${false}     | ${session([0, 1])} | ${env(false, 'instructor')} | ${true}
-    ${false}     | ${undefined}       | ${env(false, 'instructor')} | ${true}
+    ${false}     | ${session()}       | ${env(false, 'instructor')} | ${false}
+    ${false}     | ${session([0, 1])} | ${env(false, 'instructor')} | ${false}
+    ${false}     | ${undefined}       | ${env(false, 'instructor')} | ${false}
   `('1. model.lockChoiceOrder: $modelLock, $session, $env => $expected', ({ modelLock, session, env, expected }) => {
     const model = { lockChoiceOrder: modelLock };
     const result = lockChoices(model, session, env);
@@ -120,7 +120,7 @@ describe('lockChoices mod', () => {
     ${false}     | ${session()} | ${env(false)}                   | ${false}
     ${undefined} | ${session()} | ${env(true)}                    | ${true}
     ${undefined} | ${session()} | ${env(undefined)}               | ${false}
-    ${undefined} | ${session()} | ${env(undefined, 'instructor')} | ${true}
+    ${undefined} | ${session()} | ${env(undefined, 'instructor')} | ${false}
   `('2. model.lockChoiceOrder: $modelLock, $session, $env => $expected', ({ modelLock, session, env, expected }) => {
     const model = { lockChoiceOrder: modelLock };
     const result = lockChoices(model, session, env);
@@ -142,7 +142,7 @@ describe('lockChoices', () => {
     ${false}     | ${session()} | ${env(false)}                   | ${false}
     ${undefined} | ${session()} | ${env(true)}                    | ${true}
     ${undefined} | ${session()} | ${env(undefined)}               | ${false}
-    ${undefined} | ${session()} | ${env(undefined, 'instructor')} | ${true}
+    ${undefined} | ${session()} | ${env(undefined, 'instructor')} | ${false}
   `('3. model.lockChoiceOrder: $modelLock, $env => $expected', ({ modelLock, session, env, expected }) => {
     const model = { lockChoiceOrder: modelLock };
     const result = lockChoices(model, session, env);
